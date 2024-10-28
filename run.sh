cbbdb386e68ccf7e5d124fef085000aac15677e9
HuangYin0514 <1071324110@qq.com>
2024-10-28 11:12:43 +0800

    init

---
+++ run.sh
@@ -0,0 +1,13 @@
+
+
+# cd /home/hy/project/reid_multiview/v3_wL
+# sh run.sh
+
+# cd /home/hy/project/reid_multiview/v3_wQ
+# sh run.sh
+
+cd /home/hy/project/reid_multiview/v3_wQL
+sh run.sh
+
+cd /home/hy/project/reid_multiview/v4hierarchicalv2
+sh run.sh
