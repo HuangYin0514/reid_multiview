c5d2d78d29f57808b8e4a4565db9ee524c22a91c
HuangYin0514 <1071324110@qq.com>
2024-10-29 17:18:45 +0800

    1

--- v4_LP_3D/core/train.py
+++ v4_LP_3D/core/train.py
@@ -39,38 +39,29 @@ def train(base, loaders, config):
 
             features_map = base.model(imgs)
             bn_features, cls_score = base.classifier(features_map)
-
             localized_features_map, localized_integrating_features_map, integrating_pids = FeatureMapLocalizedIntegratingNoRelu(config).__call__(features_map, pids, base)
 
-            localized_bn_features, localized_cls_score = base.classifier(localized_features_map)
-
-            quantified_localized_features_map, quantified_localized_integrating_features_map, _ = FeatureMapQuantifiedIntegratingProbLogSoftmaxWeights(config).__call__(localized_features_map, localized_cls_score, pids)
-
-            quantified_localized_integrating_features_map = base.auxiliaryModel(quantified_localized_integrating_features_map)
-
-            quantified_localized_integrating_bn_features, quantified_localized_integrating_cls_score = base.classifier2(quantified_localized_integrating_features_map)
+            localized_integrating_bn_features, localized_integrating_cls_score = base.classifier2(localized_integrating_features_map)
 
             ide_loss = base.pid_creiteron(cls_score, pids)
-            quantified_localized_integrating_ide_loss = base.pid_creiteron(quantified_localized_integrating_cls_score, integrating_pids)
-            quantified_localized_integrating_reasoning_loss = base.reasoning_creiteron(bn_features, quantified_localized_integrating_bn_features)
+            localized_integrating_ide_loss = base.pid_creiteron(localized_integrating_cls_score, integrating_pids)
+            localized_integrating_reasoning_loss = base.reasoning_creiteron(bn_features, localized_integrating_bn_features)
 
-            total_loss = ide_loss + quantified_localized_integrating_ide_loss + config.lambda1 * quantified_localized_integrating_reasoning_loss
+            total_loss = ide_loss + localized_integrating_ide_loss + config.lambda1 * localized_integrating_reasoning_loss
 
             base.model_optimizer.zero_grad()
             base.classifier_optimizer.zero_grad()
             base.classifier2_optimizer.zero_grad()
-            base.auxiliaryModel_optimizer.zero_grad()
             total_loss.backward()
             base.model_optimizer.step()
             base.classifier_optimizer.step()
             base.classifier2_optimizer.step()
-            base.auxiliaryModel_optimizer.step()
 
             meter.update(
                 {
                     "pid_loss": ide_loss.data,
-                    "quantified_localized_integrating_pid_loss": quantified_localized_integrating_ide_loss.data,
-                    "quantified_localized_integrating_reasoning_loss": quantified_localized_integrating_reasoning_loss.data,
+                    "localized_integrating_pid_loss": localized_integrating_ide_loss.data,
+                    "localized_integrating_reasoning_loss": localized_integrating_reasoning_loss.data,
                 }
             )
 
