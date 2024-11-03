76f3e9b52d308c6ca8cace1e9c5d349e94078f7e
HuangYin0514 <1071324110@qq.com>
2024-11-03 11:40:33 +0800

    1

--- run.sh
+++ run.sh
@@ -1,18 +1,6 @@
 
-rm -rf /home/hy/project/reid_multiview/v4_clean/results
-cd /home/hy/project/reid_multiview/v4_clean/
-sh run.sh
-
-rm -rf /home/hy/project/reid_multiview/v4_3D_embedding_v1/results
-cd /home/hy/project/reid_multiview/v4_3D_embedding_v1
-sh run.sh
-
-rm -rf /home/hy/project/reid_multiview/v4_3D_embedding_v2/results
-cd /home/hy/project/reid_multiview/v4_3D_embedding_v2
-sh run.sh
-
-rm -rf /home/hy/project/reid_multiview/v4_3D_embedding_v3/results
-cd /home/hy/project/reid_multiview/v4_3D_embedding_v3
+rm -rf /home/hy/project/reid_multiview/v4_3D_embedding_v4/results
+cd /home/hy/project/reid_multiview/v4_3D_embedding_v4
 sh run.sh
 
 
