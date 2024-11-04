4e82063c643d55c117a1757d5f760823377b0449
HuangYin0514 <1071324110@qq.com>
2024-11-04 19:36:38 +0800

    1

--- v4_SEAM_4/network/model.py
+++ v4_SEAM_4/network/model.py
@@ -1,8 +1,9 @@
+import torch
 import torch.nn as nn
 import torchvision
 
 from .gem_pool import GeneralizedMeanPoolingP
-from .seam import SEAM
+from .resnet50 import resnet50
 
 
 def weights_init_kaiming(m):
@@ -32,19 +33,67 @@ def weights_init_classifier(m):
             nn.init.constant_(m.bias, 0.0)
 
 
+class Residual(nn.Module):
+    def __init__(self, fn):
+        super(Residual, self).__init__()
+        self.fn = fn
+
+    def forward(self, x):
+        return self.fn(x) + x
+
+
+class SEAM(nn.Module):
+    def __init__(self, c1, c2, n, reduction=16):
+        super(SEAM, self).__init__()
+        if c1 != c2:
+            c2 = c1
+        self.DCovN = nn.Sequential(
+            # nn.Conv2d(c1, c2, kernel_size=3, stride=1, padding=1, groups=c1),
+            # nn.GELU(),
+            # nn.BatchNorm2d(c2),
+            *[nn.Sequential(Residual(nn.Sequential(nn.Conv2d(in_channels=c2, out_channels=c2, kernel_size=3, stride=1, padding=1, groups=c2), nn.GELU(), nn.BatchNorm2d(c2))), nn.Conv2d(in_channels=c2, out_channels=c2, kernel_size=1, stride=1, padding=0, groups=1), nn.GELU(), nn.BatchNorm2d(c2)) for i in range(n)]
+        )
+        self.avg_pool = torch.nn.AdaptiveAvgPool2d(1)
+        self.fc = nn.Sequential(nn.Linear(c2, c2 // reduction, bias=False), nn.ReLU(inplace=True), nn.Linear(c2 // reduction, c2, bias=False), nn.Sigmoid())
+
+        self._initialize_weights()
+        # self.initialize_layer(self.avg_pool)
+        self.initialize_layer(self.fc)
+
+    def forward(self, x):
+        b, c, _, _ = x.size()
+        y = self.DCovN(x)
+        y = self.avg_pool(y).view(b, c)
+        y = self.fc(y).view(b, c, 1, 1)
+        y = torch.exp(y)
+        return x * y.expand_as(x)
+
+    def _initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.xavier_uniform_(m.weight, gain=1)
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+
+    def initialize_layer(self, layer):
+        if isinstance(layer, (nn.Conv2d, nn.Linear)):
+            torch.nn.init.normal_(layer.weight, mean=0.0, std=0.001)
+            if layer.bias is not None:
+                torch.nn.init.constant_(layer.bias, 0)
+
+
 class Model(nn.Module):
     def __init__(self):
-        super(
-            Model,
-            self,
-        ).__init__()
-        resnet = torchvision.models.resnet50(pretrained=True)
-        resnet.layer4[0].conv2.stride = (1, 1)
+        super(Model, self).__init__()
+        resnet = resnet50(pretrained=True)
+        # Modifiy backbone
         resnet.layer4[0].downsample[0].stride = (1, 1)
-
+        resnet.layer4[0].conv2.stride = (1, 1)
         # Backbone structure
         self.resnet_conv1 = resnet.conv1
         self.resnet_bn1 = resnet.bn1
+        self.resnet_relu = resnet.relu
         self.resnet_maxpool = resnet.maxpool
         self.resnet_layer1 = resnet.layer1
         self.resnet_layer2 = resnet.layer2
@@ -58,6 +107,7 @@ class Model(nn.Module):
     def forward(self, x):
         x = self.resnet_conv1(x)
         x = self.resnet_bn1(x)
+        x = self.resnet_relu(x)
         x = self.resnet_maxpool(x)
 
         x = self.resnet_layer1(x)
@@ -68,6 +118,28 @@ class Model(nn.Module):
         x = self.SEAM_3(x)
         x = self.resnet_layer4(x)
         return x
+
+
+class AuxiliaryModel(nn.Module):
+    def __init__(self, pid_num):
+        super(
+            AuxiliaryModel,
+            self,
+        ).__init__()
+
+    def forward(self, x):
+        return x
+
+
+class AuxiliaryModelClassifier(nn.Module):
+    def __init__(self, pid_num):
+        super(
+            AuxiliaryModelClassifier,
+            self,
+        ).__init__()
+
+    def forward(self, features_map):
+        return
 
 
 class Classifier(nn.Module):
@@ -113,25 +185,3 @@ class Classifier2(nn.Module):
         bn_features = self.BN(features.squeeze())
         cls_score = self.classifier(bn_features)
         return bn_features, cls_score
-
-
-class AuxiliaryModelClassifier(nn.Module):
-    def __init__(self, pid_num):
-        super(
-            AuxiliaryModelClassifier,
-            self,
-        ).__init__()
-
-    def forward(self, features_map):
-        return
-
-
-class AuxiliaryModel(nn.Module):
-    def __init__(self, pid_num):
-        super(
-            AuxiliaryModel,
-            self,
-        ).__init__()
-
-    def forward(self, x):
-        return x
