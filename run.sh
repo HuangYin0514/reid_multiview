5a1c4bde2f5718c519f820dd79419493871c4a7a
HuangYin0514 <1071324110@qq.com>
2024-11-11 16:11:25 +0800

    1

--- run.sh
+++ run.sh
@@ -21,10 +21,10 @@
 # cd /home/hy/project/reid_multiview/v4_LP_2l_2
 # sh run.sh
 
-rm -rf /home/hy/project/reid_multiview/v4_P/results
-cd /home/hy/project/reid_multiview/v4_P
+rm -rf /home/hy/project/reid_multiview/v3_P/results
+cd /home/hy/project/reid_multiview/v3_P
 sh run.sh
 
-# rm -rf /home/hy/project/reid_multiview/v4_LP_test/results
-# cd /home/hy/project/reid_multiview/v4_LP_test
+# rm -rf /home/hy/project/reid_multiview/v3_LP_format/results
+# cd /home/hy/project/reid_multiview/v3_LP_format
 # sh run.sh
