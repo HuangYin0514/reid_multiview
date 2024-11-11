dab835b36a8a6af2a61f8d73f81d839b99aa0fe3
HuangYin0514 <1071324110@qq.com>
2024-11-11 16:09:09 +0800

    1

--- run.sh
+++ run.sh
@@ -1,4 +1,7 @@
 
+#################################################################################
+# Common
+#################################################################################
 # ssh hy@10.244.37.93
 # screen -D -r training
 # conda activate py396
@@ -6,12 +9,22 @@
 # sh run.sh
 
 #################################################################################
+# Vis
+#################################################################################
+# cd /home/hy/project/reid_multiview/v3_vis
+# sh run.sh
 
+#################################################################################
+# progress
+#################################################################################
 # rm -rf /home/hy/project/reid_multiview/v4_LP_2l_2/results
 # cd /home/hy/project/reid_multiview/v4_LP_2l_2
 # sh run.sh
 
-
-
-cd /home/hy/project/reid_multiview/v3_vis
+rm -rf /home/hy/project/reid_multiview/v4_P/results
+cd /home/hy/project/reid_multiview/v4_P
 sh run.sh
+
+# rm -rf /home/hy/project/reid_multiview/v4_LP_test/results
+# cd /home/hy/project/reid_multiview/v4_LP_test
+# sh run.sh
