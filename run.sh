1fbd9ab271395e5b1185707867de3b11ea1a2f63
HuangYin0514 <1071324110@qq.com>
2024-11-18 19:47:44 +0800

    1

--- run.sh
+++ run.sh
@@ -27,33 +27,7 @@
 #################################################################################
 
 
-# # v4_IP_mv_2, format
-rm -rf /home/hy/project/reid_multiview/v4_IP_mv_12/results
-cd /home/hy/project/reid_multiview/v4_IP_mv_12
-sh run.sh
-
-
-# v4_IP_mv_2, format, bn_features = F.normalize(features, p=2, dim=1)
-rm -rf /home/hy/project/reid_multiview/v4_IP_mv_13/results
-cd /home/hy/project/reid_multiview/v4_IP_mv_13
-sh run.sh
-
-# v4_IP_mv_2, format, 共享-指定损失sum方式 
-rm -rf /home/hy/project/reid_multiview/v4_IP_mv_14/results
-cd /home/hy/project/reid_multiview/v4_IP_mv_14
-sh run.sh
-
-# v4_IP_mv_12, 共享/指定特征id约束
-rm -rf /home/hy/project/reid_multiview/v4_IP_mv_15/results
-cd /home/hy/project/reid_multiview/v4_IP_mv_15
-sh run.sh
-
-# v4_IP_mv_15, 共享/指定特征id约束，  共享-指定损失/共享损失
-rm -rf /home/hy/project/reid_multiview/v4_IP_mv_16/results
-cd /home/hy/project/reid_multiview/v4_IP_mv_16
-sh run.sh
-
-# # v4_IP_mv_16, 共享/指定特征id约束，  共享-指定损失/共享损失/指定损失
-rm -rf /home/hy/project/reid_multiview/v4_IP_mv_17/results
-cd /home/hy/project/reid_multiview/v4_IP_mv_17
+# # v4_IP_mv_3, seam注意力
+rm -rf /home/hy/project/reid_multiview/v4_IP_mv_4/results
+cd /home/hy/project/reid_multiview/v4_IP_mv_4
 sh run.sh
