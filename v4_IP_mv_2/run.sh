7f5ef5ea98e659188844e1f9ebd74f1060739dbc
HuangYin0514 <1071324110@qq.com>
2024-11-19 16:32:50 +0800

    1

--- v4_IP_mv_2/run.sh
+++ v4_IP_mv_2/run.sh
@@ -1,1 +1,1 @@
-python main.py --module Lucky --backbone resnet50 --output_path results/occluded_duke --total_train_epoch 2
+python main.py --module Lucky --backbone resnet50 --output_path results/occluded_duke
