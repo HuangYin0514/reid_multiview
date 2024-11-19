376a5a7456e50ff4fb736109227fcd5fbce8326b
HuangYin0514 <1071324110@qq.com>
2024-11-19 14:20:18 +0800

    1

--- run.sh
+++ run.sh
@@ -27,15 +27,15 @@
 #################################################################################
 
 
-# # # v4_IP_mv_3, seam注意力
-# rm -rf /home/hy/project/reid_multiview/v4_IP_mv_4/results
-# cd /home/hy/project/reid_multiview/v4_IP_mv_4
-# sh run.sh
+# # v4_IP_mv_3, seam注意力
+rm -rf /home/hy/project/reid_multiview/v4_IP_mv_4/results
+cd /home/hy/project/reid_multiview/v4_IP_mv_4
+sh run.sh
 
-# # #  # 修改了对比损失函数
-# rm -rf /home/hy/project/reid_multiview/v4_IP_mv_5/results
-# cd /home/hy/project/reid_multiview/v4_IP_mv_5
-# sh run.sh
+# #  # 修改了对比损失函数
+rm -rf /home/hy/project/reid_multiview/v4_IP_mv_5/results
+cd /home/hy/project/reid_multiview/v4_IP_mv_5
+sh run.sh
 
 # #  # v4_IP_mv_5, special view fusion
 rm -rf /home/hy/project/reid_multiview/v4_IP_mv_6/results
@@ -43,6 +43,6 @@ cd /home/hy/project/reid_multiview/v4_IP_mv_6
 sh run.sh
 
 # #  # v4_IP_mv_6, 积分方式为mean
-rm -rf /home/hy/project/reid_multiview/v4_IP_mv_6/results
-cd /home/hy/project/reid_multiview/v4_IP_mv_6
+rm -rf /home/hy/project/reid_multiview/v4_IP_mv_7/results
+cd /home/hy/project/reid_multiview/v4_IP_mv_7
 sh run.sh
