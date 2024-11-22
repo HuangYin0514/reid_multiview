34d59723fd31380e00bed6f072563da799fc9520
HuangYin0514 <1071324110@qq.com>
2024-11-22 16:05:34 +0800

    1

--- runs.sh
+++ runs.sh
@@ -28,24 +28,24 @@
 #################################################################################
 
 # #
-rm -rf /home/hy/project/reid_multiview/v4_IP_mv_5/results
-cd /home/hy/project/reid_multiview/v4_IP_mv_5
-sh run.sh
-
-# l2 distance 141
-rm -rf /home/hy/project/reid_multiview/v4_IP_mv_4/results
-cd /home/hy/project/reid_multiview/v4_IP_mv_4
-sh run.sh
-
-# flip 140
-rm -rf /home/hy/project/reid_multiview/v4_IP_mv_3/results
-cd /home/hy/project/reid_multiview/v4_IP_mv_3
-sh run.sh
-
-# # 分离特征
-# rm -rf /home/hy/project/reid_multiview/v4_IP_mv_2_backbone/results
-# cd /home/hy/project/reid_multiview/v4_IP_mv_2_backbone
+# rm -rf /home/hy/project/reid_multiview/v4_IP_mv_5/results
+# cd /home/hy/project/reid_multiview/v4_IP_mv_5
 # sh run.sh
+
+# # l2 distance 141
+# rm -rf /home/hy/project/reid_multiview/v4_IP_mv_4/results
+# cd /home/hy/project/reid_multiview/v4_IP_mv_4
+# sh run.sh
+
+# # flip 140
+# rm -rf /home/hy/project/reid_multiview/v4_IP_mv_3/results
+# cd /home/hy/project/reid_multiview/v4_IP_mv_3
+# sh run.sh
+
+# 分离特征
+rm -rf /home/hy/project/reid_multiview/v4_IP_mv_2_backbone/results
+cd /home/hy/project/reid_multiview/v4_IP_mv_2_backbone
+sh run.sh
 
 
 
---
+++ v4_IP_mv_3/core/__init__.py
@@ -0,0 +1,4 @@
+from .base import *
+from .test import *
+from .train import *
+from .visualization import visualization
