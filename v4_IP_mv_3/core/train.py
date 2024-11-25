28aa542315708ff95b25dcde5b58c250d01b0d09
HuangYin0514 <1071324110@qq.com>
2024-11-25 15:17:30 +0800

    1

---
+++ v4_IP_mv_3/core/train.py
@@ -0,0 +1,19 @@
+from tools import MultiItemAverageMeter
+from tqdm import tqdm
+
+
+def train(base, loaders, config):
+    base.set_train()
+    loader = loaders.loader
+    meter = MultiItemAverageMeter()
+    for epoch, data in enumerate(tqdm(loader)):
+        imgs, pids, cids = data
+        imgs, pids, cids = imgs.to(base.device), pids.to(base.device).long(), cids.to(base.device).long()
+        if config.module == "Lucky":
+            total_loss = base.model(imgs, pids, cids, epoch, meter)
+
+            base.model_optimizer.zero_grad()
+            total_loss.backward()
+            base.model_optimizer.step()
+
+    return meter.get_val(), meter.get_str()
