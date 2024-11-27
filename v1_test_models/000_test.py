37780de7dafa86b1a056a6f1e4a5410c8e29eaa8
HuangYin0514 <1071324110@qq.com>
2024-11-27 19:53:11 +0800

    1

--- v1_test_models/000_test.py
+++ v1_test_models/000_test.py
@@ -1,14 +1,17 @@
 import torch
 
-matrix = torch.range(1, 9).view(3, 3)
-print("Original matrix:\n", matrix)
+if __name__ == "__main__":
+    print("main")
 
-bs = matrix.size(0)
-mask = ~torch.eye(bs, dtype=torch.bool)
-non_diag_elements = matrix[mask]
-print("Non-diagonal elements:\n", non_diag_elements)
+    matrix = torch.range(1, 9).view(3, 3)
+    print("Original matrix:\n", matrix)
 
-bs = matrix.size(0)
-mask = torch.eye(bs, dtype=torch.bool)
-non_diag_elements = matrix[mask]
-print("diagonal elements:\n", non_diag_elements)
+    bs = matrix.size(0)
+    mask = ~torch.eye(bs, dtype=torch.bool)
+    non_diag_elements = matrix[mask]
+    print("Non-diagonal elements:\n", non_diag_elements)
+
+    bs = matrix.size(0)
+    mask = torch.eye(bs, dtype=torch.bool)
+    non_diag_elements = matrix[mask]
+    print("diagonal elements:\n", non_diag_elements)
