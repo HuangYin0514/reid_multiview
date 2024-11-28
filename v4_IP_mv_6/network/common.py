b006f037885b732b7ebaeabd4375a36024d6199f
HuangYin0514 <1071324110@qq.com>
2024-11-28 20:37:20 +0800

    1

---
+++ v4_IP_mv_6/network/common.py
@@ -0,0 +1,122 @@
+import torch.nn as nn
+import torchvision
+
+from .gem_pool import GeneralizedMeanPoolingP
+
+
+def weights_init_kaiming(m):
+    classname = m.__class__.__name__
+    if classname.find("Linear") != -1:
+        nn.init.kaiming_normal_(m.weight, a=0, mode="fan_out")
+        if m.bias is not None:
+            nn.init.constant_(m.bias, 0)
+    elif classname.find("Conv") != -1:
+        nn.init.kaiming_normal_(m.weight, a=0, mode="fan_in")
+        if m.bias is not None:
+            nn.init.constant_(m.bias, 0.0)
+    elif classname.find("BatchNorm") != -1:
+        if m.affine:
+            nn.init.constant_(m.weight, 1.0)
+            nn.init.constant_(m.bias, 0.0)
+    elif classname.find("InstanceNorm") != -1:
+        if m.affine:
+            nn.init.constant_(m.weight, 1.0)
+            nn.init.constant_(m.bias, 0.0)
+
+
+def weights_init_classifier(m):
+    classname = m.__class__.__name__
+    if classname.find("Linear") != -1:
+        nn.init.normal_(m.weight, std=0.001)
+        if m.bias:
+            nn.init.constant_(m.bias, 0.0)
+
+
+class MLPResidualBlock(nn.Module):
+    def __init__(self, in_channels, num_layers=1):
+        super(MLPResidualBlock, self).__init__()
+        net = nn.ModuleList()
+        for _ in range(num_layers):
+            mlp = nn.Sequential(
+                nn.Linear(in_channels, in_channels, bias=False),
+                nn.BatchNorm1d(in_channels),
+            )
+            net.append(mlp)
+        self.net = net
+        self.net.apply(weights_init_kaiming)
+
+    def forward(self, x):
+        identity = x
+        for mlp in self.net:
+            out = mlp(x)
+            x = out
+        out += identity
+        return out
+
+
+class GAP_BN(nn.Module):
+    def __init__(self, channel=2048):
+        super(GAP_BN, self).__init__()
+        self.GAP = GeneralizedMeanPoolingP()
+        # self.GAP = nn.AdaptiveAvgPool2d(1)
+        self.BN = nn.BatchNorm1d(channel)
+        self.BN.apply(weights_init_kaiming)
+
+    def forward(self, features_map):
+        features = self.GAP(features_map)
+        bn_features = self.BN(features.squeeze())
+        return bn_features
+
+
+class BN_Classifier(nn.Module):
+    def __init__(self, channel=2048, pid_num=None):
+        super(BN_Classifier, self).__init__()
+        self.pid_num = pid_num
+        self.BN = nn.BatchNorm1d(channel)
+        self.BN.apply(weights_init_kaiming)
+        self.classifier = nn.Linear(channel, self.pid_num, bias=False)
+        self.classifier.apply(weights_init_classifier)
+
+    def forward(self, features):
+        bn_features = self.BN(features.squeeze())
+        cls_score = self.classifier(bn_features)
+        return bn_features, cls_score
+
+
+class Classifier(nn.Module):
+    def __init__(self, pid_num):
+        super(Classifier, self).__init__()
+        self.pid_num = pid_num
+        self.GAP = GeneralizedMeanPoolingP()
+        self.BN = nn.BatchNorm1d(2048)
+        self.BN.apply(weights_init_kaiming)
+
+        self.classifier = nn.Linear(2048, self.pid_num, bias=False)
+        self.classifier.apply(weights_init_classifier)
+
+    def forward(self, features_map):
+        features = self.GAP(features_map)
+        bn_features = self.BN(features.squeeze())
+        cls_score = self.classifier(bn_features)
+        if self.training:
+            return bn_features, cls_score
+        else:
+            return bn_features
+
+
+class Classifier2(nn.Module):
+    def __init__(self, pid_num):
+        super(Classifier2, self).__init__()
+        self.pid_num = pid_num
+        self.GAP = GeneralizedMeanPoolingP()
+        self.BN = nn.BatchNorm1d(2048)
+        self.BN.apply(weights_init_kaiming)
+
+        self.classifier = nn.Linear(2048, self.pid_num, bias=False)
+        self.classifier.apply(weights_init_classifier)
+
+    def forward(self, features_map):
+        features = self.GAP(features_map)
+        bn_features = self.BN(features.squeeze())
+        cls_score = self.classifier(bn_features)
+        return bn_features, cls_score
