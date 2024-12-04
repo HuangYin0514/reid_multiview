55c0cb45cd72793c49942aeff3e66532b21c41e1
HuangYin0514 <1071324110@qq.com>
2024-12-04 20:26:50 +0800

    1

---
+++ v4_IP_mv_2/network/model.py
@@ -0,0 +1,45 @@
+import torch
+import torch.nn as nn
+from tools import CrossEntropyLabelSmooth
+
+from .common import *
+from .contrastive_loss import *
+
+
+class Model(nn.Module):
+
+    def __init__(self, config):
+        super(Model, self).__init__()
+        self.backbone = Backbone()
+
+        # 分类
+        self.gap_bn = GAP_BN(2048)
+        self.bn_classifier = BN_Classifier(2048, config.pid_num)
+        self.bn_classifier2 = BN_Classifier(2048, config.pid_num)
+
+        # 解耦
+        self.decoupling = FeatureDecoupling(config)
+        self.decoupling_shared_bn_classifier = BN_Classifier(1024, config.pid_num)
+        self.decoupling_special_bn_classifier = BN_Classifier(1024, config.pid_num)
+
+        # 特征融合
+        self.feature_integrating = FeatureMapIntegrating(config)
+
+    def heatmap(self, x):
+        _, _, _, _, features_map = self.backbone(x)
+        return features_map
+
+    def forward(self, x):
+        if self.training:
+            _, _, _, _, features_map = self.backbone(x)
+            return features_map
+        else:
+            _, _, _, _, features_map = self.backbone(x)
+            bn_features = self.gap_bn(features_map)
+
+            flip_x = torch.flip(x, [3])
+            _, _, _, _, flip_features_map = self.backbone(flip_x)
+            flip_bn_features = self.gap_bn(flip_features_map)
+
+            bn_features = bn_features + flip_bn_features
+            return bn_features
