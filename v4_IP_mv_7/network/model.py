4d31b44aa111cc177ee6e0be6f8c684d73935032
HuangYin0514 <1071324110@qq.com>
2024-12-04 15:25:59 +0800

    1

--- v4_IP_mv_7/network/model.py
+++ v4_IP_mv_7/network/model.py
@@ -4,40 +4,6 @@ from tools import CrossEntropyLabelSmooth
 
 from .common import *
 from .contrastive_loss import *
-from .resnet50 import resnet50
-
-
-class Backbone(nn.Module):
-    def __init__(self):
-        super(Backbone, self).__init__()
-        resnet = resnet50(pretrained=True)
-        # resnet = torchvision.models.resnet50(pretrained=True)
-        # Modifiy backbone
-        resnet.layer4[0].downsample[0].stride = (1, 1)
-        resnet.layer4[0].conv2.stride = (1, 1)
-        # Backbone structure
-        self.resnet_conv1 = resnet.conv1
-        self.resnet_bn1 = resnet.bn1
-        self.resnet_maxpool = resnet.maxpool
-        self.resnet_layer1 = resnet.layer1
-        self.resnet_layer2 = resnet.layer2
-        self.resnet_layer3 = resnet.layer3
-        self.resnet_layer4 = resnet.layer4
-
-    def forward(self, x):
-        x = self.resnet_conv1(x)
-        x = self.resnet_bn1(x)
-        x = self.resnet_maxpool(x)
-
-        x1 = x
-        x = self.resnet_layer1(x)
-        x2 = x
-        x = self.resnet_layer2(x)
-        x3 = x
-        x = self.resnet_layer3(x)
-        x4 = x
-        x = self.resnet_layer4(x)
-        return x1, x2, x3, x4, x
 
 
 class Model(nn.Module):
@@ -62,32 +28,6 @@ class Model(nn.Module):
     def heatmap(self, x):
         _, _, _, _, features_map = self.backbone(x)
         return features_map
-
-    def make_loss(self, input_features, pids, cids, epoch, meter):
-
-        (features,) = input_features
-
-        # IDE
-        bn_features, cls_score = self.bn_classifier(features)
-        ide_loss = CrossEntropyLabelSmooth().forward(cls_score, pids)
-
-        # 多视角
-        integrating_features, integrating_pids = self.feature_integrating(features, pids)
-        integrating_bn_features, integrating_cls_score = self.bn_classifier2(integrating_features)
-        integrating_ide_loss = CrossEntropyLabelSmooth().forward(integrating_cls_score, integrating_pids)
-        integrating_reasoning_loss = ReasoningLoss().forward(bn_features, integrating_bn_features)
-
-        # 总损失
-        total_loss = ide_loss + integrating_ide_loss + 0.007 * integrating_reasoning_loss
-
-        meter.update(
-            {
-                "pid_loss": ide_loss.data,
-                "integrating_pid_loss": integrating_ide_loss.data,
-                "integrating_reasoning_loss": integrating_reasoning_loss.data,
-            }
-        )
-        return total_loss
 
     def forward(self, x):
         if self.training:
