201dc0d494641cacbb9fcb6836b220d868a371b9
HuangYin0514 <1071324110@qq.com>
2024-12-04 18:14:01 +0800

    1

--- v4_IP_mv_1/core/train.py
+++ v4_IP_mv_1/core/train.py
@@ -1,4 +1,5 @@
-from network.common import ReasoningLoss
+from network.common import *
+from network.feature_map_processing import *
 from tools import CrossEntropyLabelSmooth, MultiItemAverageMeter
 from tqdm import tqdm
 
@@ -7,6 +8,9 @@ def train(base, loaders, config):
     base.set_train()
     loader = loaders.loader
     meter = MultiItemAverageMeter()
+    config.lower = 0.02
+    config.upper = 0.4
+    config.ratio = 0.3
     for epoch, data in enumerate(tqdm(loader)):
         imgs, pids, cids = data
         imgs, pids, cids = imgs.to(base.device), pids.to(base.device).long(), cids.to(base.device).long()
@@ -14,12 +18,30 @@ def train(base, loaders, config):
             features_map = base.model(imgs)
             bn_features = base.model.module.gap_bn(features_map)
 
+            erasing_features_map = FeatureMapErasing(config).__call__(features_map)
+            erasing_bn_features = base.model.module.gap_bn(erasing_features_map)
+
+            noising_features_map = FeatureMapNoising(config).__call__(features_map)
+            noising_bn_features = base.model.module.gap_bn(noising_features_map)
+
+            transforming_features_map = FeatureMapTransforming(config).__call__(features_map)
+            transforming_bn_features = base.model.module.gap_bn(transforming_features_map)
+
             # IDE
             bn_features, cls_score = base.model.module.bn_classifier(bn_features)
             ide_loss = CrossEntropyLabelSmooth().forward(cls_score, pids)
 
+            erasing_bn_features, cls_score = base.model.module.bn_classifier(erasing_bn_features)
+            erasing_loss = CrossEntropyLabelSmooth().forward(cls_score, pids)
+
+            noising_bn_features, cls_score = base.model.module.bn_classifier(noising_bn_features)
+            noising_loss = CrossEntropyLabelSmooth().forward(cls_score, pids)
+
+            transforming_bn_features, cls_score = base.model.module.bn_classifier(transforming_bn_features)
+            transforming_loss = CrossEntropyLabelSmooth().forward(cls_score, pids)
+
             # 总损失
-            total_loss = ide_loss
+            total_loss = ide_loss + 0.1 * erasing_loss + 0.15 * noising_loss + 0.1 * transforming_loss
 
             # 反向传播
             base.model_optimizer.zero_grad()
