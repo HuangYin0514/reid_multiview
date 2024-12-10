37777ae2c1523209390f315aaa917522a9778e9d
HuangYin0514 <1071324110@qq.com>
2024-12-10 10:11:18 +0800

    1

--- v4_IP_mv_1/network/common.py
+++ v4_IP_mv_1/network/common.py
@@ -97,6 +97,10 @@ class FeatureDecoupling(nn.Module):
         self.mlp1 = nn.Sequential(
             nn.Linear(ic, oc, bias=False),
             nn.BatchNorm1d(oc),
+            nn.Linear(oc, oc, bias=False),
+            nn.BatchNorm1d(oc),
+            nn.Linear(oc, oc, bias=False),
+            nn.BatchNorm1d(oc),
         )
         self.mlp1.apply(weights_init_kaiming)
 
