ac32bb999628f985b25fbc689154af26b4f55291
HuangYin0514 <1071324110@qq.com>
2024-12-21 07:38:06 +0800

    1

--- v4_IP_mv_1/core/train.py
+++ v4_IP_mv_1/core/train.py
@@ -1,5 +1,8 @@
 from network.contrastive_loss import *
-from network.processing import FeatureMapLocalizedIntegratingNoRelu
+from network.processing import (
+    FeatureMapLocalizedIntegratingNoRelu,
+    FeatureMapQuantifiedIntegratingProbLogSoftmaxWeights,
+)
 from tools import CrossEntropyLabelSmooth, KLDivLoss, MultiItemAverageMeter
 from tqdm import tqdm
 
@@ -12,14 +15,22 @@ def train(base, loaders, config):
         imgs, pids, cids = data
         imgs, pids, cids = imgs.to(base.device), pids.to(base.device).long(), cids.to(base.device).long()
         if config.module == "Lucky":
+            # Baseline
             features_map = base.model(imgs)
             bn_features = base.model.module.gap_bn(features_map)
             bn_features, cls_score = base.model.module.bn_classifier(bn_features)
 
-            localized_features_map, localized_integrating_features_map, integrating_pids = FeatureMapLocalizedIntegratingNoRelu(config).__call__(features_map, pids, base.model.module.bn_classifier)
-            localized_integrating_bn_features = base.model.module.gap_bn(localized_integrating_features_map)
-            localized_integrating_bn_features, localized_integrating_cls_score = base.model.module.bn_classifier2(localized_integrating_bn_features)
+            # 定位
+            localized_features_map = FeatureMapLocalizedIntegratingNoRelu(config).__call__(features_map, pids, base.model.module.bn_classifier)
+            bn_localized_features = base.model.module.gap_bn(localized_features_map)
+            _, localized_cls_score = base.model.module.bn_classifier2(bn_localized_features)
 
+            # 量化
+            quantified_integrating_features_map, integrating_pids = FeatureMapQuantifiedIntegratingProbLogSoftmaxWeights(config).__call__(localized_features_map, localized_cls_score, pids)
+            bn_quantified_integrating_features = base.model.module.gap_bn(quantified_integrating_features_map)
+            localized_integrating_bn_features, localized_integrating_cls_score = base.model.module.bn_classifier2(bn_quantified_integrating_features)
+
+            # Loss
             ide_loss = CrossEntropyLabelSmooth().forward(cls_score, pids)
             localized_integrating_ide_loss = CrossEntropyLabelSmooth().forward(localized_integrating_cls_score, integrating_pids)
             localized_integrating_reasoning_loss = ReasoningLoss().forward(bn_features, localized_integrating_bn_features)
