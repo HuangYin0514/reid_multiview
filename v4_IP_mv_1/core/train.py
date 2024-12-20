f4ae5056be3e4f1d42108ac57c86b8b4172b9d28
HuangYin0514 <1071324110@qq.com>
2024-12-21 00:15:41 +0800

    1

--- v4_IP_mv_1/core/train.py
+++ v4_IP_mv_1/core/train.py
@@ -13,14 +13,16 @@ def train(base, loaders, config):
         imgs, pids, cids = imgs.to(base.device), pids.to(base.device).long(), cids.to(base.device).long()
         if config.module == "Lucky":
             features_map = base.model(imgs)
-            bn_features, cls_score = base.model.module.classifier(features_map)
+            bn_features = base.model.module.gap_bn(features_map)
+            bn_features, cls_score = base.model.module.bn_classifier(bn_features)
 
-            localized_features_map, localized_integrating_features_map, integrating_pids = FeatureMapLocalizedIntegratingNoRelu(config).__call__(features_map, pids, base)
-            localized_integrating_bn_features, localized_integrating_cls_score = base.model.module.classifier2(localized_integrating_features_map)
+            localized_features_map, localized_integrating_features_map, integrating_pids = FeatureMapLocalizedIntegratingNoRelu(config).__call__(features_map, pids, base.model.module.bn_classifier)
+            localized_integrating_bn_features = base.model.module.gap_bn(localized_integrating_features_map)
+            localized_integrating_bn_features, localized_integrating_cls_score = base.model.module.bn_classifier2(localized_integrating_bn_features)
 
             ide_loss = CrossEntropyLabelSmooth().forward(cls_score, pids)
             localized_integrating_ide_loss = CrossEntropyLabelSmooth().forward(localized_integrating_cls_score, integrating_pids)
-            localized_integrating_reasoning_loss = ReasoningLoss.forward(bn_features, localized_integrating_bn_features)
+            localized_integrating_reasoning_loss = ReasoningLoss().forward(bn_features, localized_integrating_bn_features)
 
             total_loss = ide_loss + localized_integrating_ide_loss + 0.007 * localized_integrating_reasoning_loss
 
--- v4_IP_mv_1/network/processing.py
+++ v4_IP_mv_1/network/processing.py
@@ -6,13 +6,13 @@ class FeatureMapLocalizedIntegratingNoRelu:
         super(FeatureMapLocalizedIntegratingNoRelu, self).__init__()
         self.config = config
 
-    def __call__(self, features_map, pids, base):
+    def __call__(self, features_map, pids, classifier):
         size = features_map.size(0)
         c, h, w = features_map.size(1), features_map.size(2), features_map.size(3)
         chunk_size = int(size / 4)
 
         # Heatmaps
-        classifier_params = [param for name, param in base.classifier.named_parameters()]
+        classifier_params = [param for name, param in classifier.named_parameters()]
         heatmaps = torch.zeros((size, h, w), device="cuda")
         for i in range(size):
             heatmap_i = torch.matmul(classifier_params[-1][pids[i]].unsqueeze(0), features_map[i].unsqueeze(0).reshape(c, h * w)).detach()
@@ -23,7 +23,6 @@ class FeatureMapLocalizedIntegratingNoRelu:
 
         # Localized
         localized_features_map = features_map * heatmaps.unsqueeze(1).clone().detach()
-        localized_features_map = base.auxiliaryModel(localized_features_map)
 
         # Fusion
         localized_integrating_features_map = localized_features_map.view(chunk_size, 4, c, h, w)  # (chunk_size, 4, c, h, w)
