6b27c0d1c7117a8ff4cafc5645f452d683ab85d7
HuangYin0514 <1071324110@qq.com>
2024-12-24 16:10:12 +0800

    1

--- v4_IP_mv_1/network/contrastive_loss.py
+++ v4_IP_mv_1/network/contrastive_loss.py
@@ -52,6 +52,15 @@ class ReasoningLoss(nn.Module):
         super(ReasoningLoss, self).__init__()
 
     def forward(self, bn_features, bn_features2):
+        loss = torch.norm((bn_features), p=2)
+        return loss
+
+
+class ReasoningLoss_ori(nn.Module):
+    def __init__(self):
+        super(ReasoningLoss, self).__init__()
+
+    def forward(self, bn_features, bn_features2):
         new_bn_features2 = torch.zeros(bn_features.size()).cuda()
         for i in range(int(bn_features2.size(0) / 4)):
             new_bn_features2[i * 4 : i * 4 + 4] = bn_features2[i]
