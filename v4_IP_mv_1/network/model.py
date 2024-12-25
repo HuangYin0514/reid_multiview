1da5af7dc555382021c62c43e6c747acc78d7483
HuangYin0514 <1071324110@qq.com>
2024-12-25 16:32:50 +0800

    1

--- v4_IP_mv_1/network/model.py
+++ v4_IP_mv_1/network/model.py
@@ -3,6 +3,8 @@ import torch.nn as nn
 
 from .common import *
 from .gem_pool import GeneralizedMeanPoolingP
+from .resnet50 import resnet50
+from .resnet_ibn_a import resnet50_ibn_a
 
 
 class Classifier(nn.Module):
