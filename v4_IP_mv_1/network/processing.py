c360307c1b8fd2ed185c2dea777bc4d6521d2dfe
HuangYin0514 <1071324110@qq.com>
2024-12-26 18:02:28 +0800

    1

--- v4_IP_mv_1/network/processing.py
+++ v4_IP_mv_1/network/processing.py
@@ -27,10 +27,7 @@ class FeatureMapLocation:
             heatmap_i = torch.tensor(heatmap_i)
             heatmaps[i, :, :] = heatmap_i
 
-        heatmaps = heatmaps.cuda()
-
-        for i in range(size):
-            localized_features_map[i] = features_map[i] * heatmaps[i]
+        localized_features_map = features_map * heatmaps.unsqueeze(1).clone().detach()
 
         return localized_features_map
 
@@ -43,20 +40,9 @@ class FeatureMapQuantification:
     def __call__(self, features_map, cls_scores, pids):
         size = features_map.size(0)
         prob = torch.log_softmax(cls_scores, dim=1)
-        probs = []
-        for i in range(size):
-            probs.append(prob[i][pids[i]])
-        probs = torch.tensor(probs, dtype=torch.float).cuda()
-        chunk_size = int(size / 4)  # 16
-        chunk_probs = torch.chunk(probs, chunks=chunk_size, dim=0)
-        chunk_weights = []
-        for chunk_prob in chunk_probs:
-            chunk_weight = torch.softmax(chunk_prob, dim=0)
-            chunk_weights.append(chunk_weight)
-        weights = torch.cat(chunk_weights, dim=0)
-        quantified_features_map = torch.zeros(features_map.size()).cuda()
-        for i in range(size):
-            quantified_features_map[i] = weights[i] * features_map[i]
+        probs = prob[torch.arange(size), pids]
+        weights = torch.softmax(probs.view(-1, 4), dim=1).view(-1).clone().detach()
+        quantified_features_map = weights.unsqueeze(1).unsqueeze(2).unsqueeze(3) * features_map
         return quantified_features_map
 
 
@@ -69,6 +55,7 @@ class FeatureMapIntegration:
         size = features_map.size(0)
         chunk_size = int(size / 4)  # 16
         c, h, w = features_map.size(1), features_map.size(2), features_map.size(3)
+
         chunk_features_map = torch.chunk(features_map, chunks=chunk_size, dim=0)
         chunk_pids = torch.chunk(pids, chunks=chunk_size, dim=0)
         integrating_features_map = torch.zeros([chunk_size, c, h, w]).cuda()
