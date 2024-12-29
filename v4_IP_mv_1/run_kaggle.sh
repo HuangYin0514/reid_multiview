e09694e2ba7263fb0e10c546fa235cc24db8c8b7
HuangYin0514 <1071324110@qq.com>
2024-12-29 16:50:16 +0800

    1

--- v4_IP_mv_1/run_kaggle.sh
+++ v4_IP_mv_1/run_kaggle.sh
@@ -1,3 +1,3 @@
 # wandb login 3f48bf3a46896db0d115e8dca2f400fee4504de6
 
-python main.py --module Lucky --backbone resnet50 --output_path results/occluded_duke --occluded_duke_path /kaggle/input/occluded-duke/Occluded_Duke
+python main.py --module Lucky --backbone resnet50 --output_path results/occluded_duke --occluded_duke_path /kaggle/input/occluded-duke/Occluded_Duke --total_train_epoch 10
