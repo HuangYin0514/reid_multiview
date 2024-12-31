4ff7240d692513e03ab21b6937cfb773b8489636
HuangYin0514 <1071324110@qq.com>
2024-12-31 17:02:46 +0800

    1

--- v4_IP_mv_1/run_kaggle.sh
+++ v4_IP_mv_1/run_kaggle.sh
@@ -1,6 +1,6 @@
 wandb login c74133df8c2cf575304acf8a99fe03ab74b6fe6a
 
-python main.py --mode train --output_path results/occluded_duke --occluded_duke_path /kaggle/input/occluded-duke/Occluded_Duke --pid_num 702 --module Lucky --task_name v296 --notes 对比, resnet50 --tags dev resnet50 occluded_duke
+python main.py --mode train --output_path results/occluded_duke --occluded_duke_path /kaggle/input/occluded-duke/Occluded_Duke --pid_num 702 --module Lucky --task_name v296 --notes 对比,resnet50 --tags dev resnet50 occluded_duke
 
 # python main.py --mode visualization --output_path results/occluded_duke --occluded_duke_path /kaggle/input/occluded-duke/Occluded_Duke --pid_num 702 --module Lucky
 # tar -czf occluded_duke_vis.tar.gz results/occluded_duke 
