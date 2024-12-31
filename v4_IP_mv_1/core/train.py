6c21c65a67d36a32e661700b695bd85a6945a57d
HuangYin0514 <1071324110@qq.com>
2024-12-31 11:39:45 +0800

    1

--- v4_IP_mv_1/core/train.py
+++ v4_IP_mv_1/core/train.py
@@ -26,14 +26,45 @@ def train(base, loaders, config):
             #################################################################
             # 量化
             quantified_features_map = FeatureMapQuantification(config).__call__(localized_features_map, localized_cls_score, pids)
-            integrating_features_map, integrating_pids = FeatureMapIntegration(config).__call__(quantified_features_map, pids)
-            localized_integrating_bn_features, localized_integrating_cls_score = base.model.module.classifier2(integrating_features_map)
-            localized_integrating_ide_loss = CrossEntropyLabelSmooth().forward(localized_integrating_cls_score, integrating_pids)
-            localized_integrating_reasoning_loss = ReasoningLoss().forward(bn_features, localized_integrating_bn_features)
+
+            #################################################################
+            # 解耦
+            bn_quantified_features = base.model.module.gap_bn(quantified_features_map)
+            shared_features, special_features = base.model.module.decoupling(bn_quantified_features)
+            shared_special_features = base.model.module.decoupling_fusion(shared_features, special_features)
+
+            # 3个损失
+            # 共享特征损失
+            _, shared_cls_score = base.model.module.decoupling_shared_classifier(shared_features)
+            shared_ide_loss = CrossEntropyLabelSmooth().forward(shared_cls_score, pids)
+            # 指定特征损失
+            _, special_cls_score = base.model.module.decoupling_special_classifier(special_features)
+            special_ide_loss = CrossEntropyLabelSmooth().forward(special_cls_score, pids)
+            # 融合特征损失
+            _, fusion_cls_score = base.model.module.decoupling_fusion_classifier(shared_special_features)
+            fusion_ide_loss = CrossEntropyLabelSmooth().forward(fusion_cls_score, pids)
+            decoupling_loss = shared_ide_loss + special_ide_loss + fusion_ide_loss
+
+            reasoning_loss = FeatureRegularizationLoss().forward(bn_features)
+
+            num_views = 4
+            bs = cls_score.size(0)
+            chunk_bs = int(bs / num_views)
+            decoupling_ss_loss = 0
+            for i in range(chunk_bs):
+                shared_feature_i = shared_features[num_views * i : num_views * (i + 1), ...]
+                special_feature_i = special_features[num_views * i : num_views * (i + 1), ...]
+                # (共享-指定)损失
+                sharedSpecialLoss = SharedSpecialLoss().forward(shared_feature_i, special_feature_i)
+                # (共享)损失
+                # sharedSharedLoss = SharedSharedLoss().forward(shared_feature_i)
+                # (指定)损失
+                # specialSpecialLoss = SpecialSpecialLoss().forward(special_feature_i)
+                decoupling_ss_loss += sharedSpecialLoss
 
             #################################################################
             # Loss
-            total_loss = ide_loss + localized_integrating_ide_loss + 0.007 * localized_integrating_reasoning_loss
+            total_loss = ide_loss + decoupling_loss / (decoupling_loss / ide_loss).detach() + reasoning_loss / (reasoning_loss / ide_loss).detach() + decoupling_ss_loss / (decoupling_ss_loss / ide_loss).detach()
 
             base.model_optimizer.zero_grad()
             total_loss.backward()
@@ -42,8 +73,9 @@ def train(base, loaders, config):
             meter.update(
                 {
                     "pid_loss": ide_loss.data,
-                    "localized_integrating_ide_loss": localized_integrating_ide_loss.data,
-                    "localized_integrating_reasoning_loss": localized_integrating_reasoning_loss.data,
+                    "decoupling_loss": decoupling_loss.data,
+                    "reasoning_loss": reasoning_loss.data,
+                    "decoupling_ss_loss": decoupling_ss_loss.data,
                 }
             )
 
--- v4_IP_mv_1/network/model.py
+++ v4_IP_mv_1/network/model.py
@@ -7,6 +7,62 @@ from .resnet50 import resnet50
 from .resnet_ibn_a import resnet50_ibn_a
 
 
+class FeatureFusion(nn.Module):
+    def __init__(self, config):
+        super(FeatureFusion, self).__init__()
+        self.config = config
+        self.BN = nn.BatchNorm1d(2048)
+        self.BN.apply(weights_init_kaiming)
+
+    def __call__(self, features_1, features_2):
+        bs = features_1.size(0)
+        out = torch.cat([features_1, features_2], dim=1)
+        out = self.BN(out)
+        return out
+
+
+class BN_Classifier(nn.Module):
+    def __init__(self, channel=2048, pid_num=None):
+        super(BN_Classifier, self).__init__()
+        self.pid_num = pid_num
+        self.BN = nn.BatchNorm1d(channel)
+        self.BN.apply(weights_init_kaiming)
+        self.classifier = nn.Linear(channel, self.pid_num, bias=False)
+        self.classifier.apply(weights_init_classifier)
+
+    def forward(self, features):
+        bn_features = self.BN(features.squeeze())
+        cls_score = self.classifier(bn_features)
+        return bn_features, cls_score
+
+
+class FeatureDecoupling(nn.Module):
+    def __init__(self, config):
+        super(FeatureDecoupling, self).__init__()
+        self.config = config
+
+        # shared branch
+        ic = 2048
+        oc = 1024
+        self.mlp1 = nn.Sequential(
+            nn.Linear(ic, oc, bias=False),
+            nn.BatchNorm1d(oc),
+        )
+        self.mlp1.apply(weights_init_kaiming)
+
+        # special branch
+        self.mlp2 = nn.Sequential(
+            nn.Linear(ic, oc, bias=False),
+            nn.BatchNorm1d(oc),
+        )
+        self.mlp2.apply(weights_init_kaiming)
+
+    def forward(self, features):
+        shared_features = self.mlp1(features)
+        special_features = self.mlp2(features)
+        return shared_features, special_features
+
+
 class Classifier(nn.Module):
     def __init__(self, c_dim, pid_num):
         super(Classifier, self).__init__()
@@ -66,8 +122,20 @@ class Model(nn.Module):
         super(Model, self).__init__()
 
         self.backbone = Backbone()
+
+        # 分类器
         self.classifier = Classifier(2048, config.pid_num)
+
+        # 多视角分类器
         self.classifier2 = Classifier(2048, config.pid_num)
+
+        # 特征解耦
+        self.gap_bn = GAP_BN(2048)
+        self.decoupling = FeatureDecoupling(config)
+        self.decoupling_fusion = FeatureFusion(config)
+        self.decoupling_shared_classifier = BN_Classifier(1024, config.pid_num)
+        self.decoupling_special_classifier = BN_Classifier(1024, config.pid_num)
+        self.decoupling_fusion_classifier = BN_Classifier(2048, config.pid_num)
 
     def heatmap(self, x):
         _, _, _, _, features_map = self.backbone(x)
