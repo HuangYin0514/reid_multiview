e0d733b36dc0502f57b76c6e3e870a829dd16729
HuangYin0514 <1071324110@qq.com>
2025-01-03 15:00:19 +0800

    1

--- v4_IP_mv_1/network/model.py
+++ v4_IP_mv_1/network/model.py
@@ -169,13 +169,13 @@ class Model(nn.Module):
             return features_map
         else:
             ###############
-            # x1, x2, x3, x4, features_map = self.backbone(x)
-            # bn_features, cls_score = self.pclassifier(features_map)
+            x1, x2, x3, x4, features_map = self.backbone(x)
+            bn_features, cls_score = self.pclassifier(features_map)
 
             ###############
-            x1, x2, x3, x4, features_map = self.backbone(x)
-            global_features = self.decoupling_gap_bn(features_map)
-            shared_features, special_features = self.featureDecoupling(global_features)
-            reconstructed_features = self.featureReconstruction(shared_features, special_features)
-            bn_features, cls_score = self.classifier(reconstructed_features)
+            # x1, x2, x3, x4, features_map = self.backbone(x)
+            # global_features = self.decoupling_gap_bn(features_map)
+            # shared_features, special_features = self.featureDecoupling(global_features)
+            # reconstructed_features = self.featureReconstruction(shared_features, special_features)
+            # bn_features, cls_score = self.classifier(reconstructed_features)
             return bn_features
