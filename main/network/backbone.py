b2e1e7a5b7afb6a2f4f0d912c093473d3b149587
hy1071324110 <hy1071324110@gmail.com>
2025-02-18 01:17:00 +0800

    v419

--- main/network/backbone.py
+++ main/network/backbone.py
@@ -7,7 +7,7 @@ from .net_module import resnet50, resnet50_ibn_a, vit_base_patch16_224_TransReID
 class Backbone(nn.Module):
     def __init__(self):
         super(Backbone, self).__init__()
-        model = vit_base_patch16_224_TransReID(img_size=[256, 128], sie_xishu=3.0, local_feature=False, camera=0, view=0, stride_size=[16, 16], drop_path_rate=0.1, drop_rate=0.0, attn_drop_rate=0.0)
+        model = vit_base_patch16_224_TransReID(img_size=[256, 128], sie_xishu=3.0, local_feature=False, camera=0, view=0, stride_size=[11, 11], drop_path_rate=0.1, drop_rate=0.0, attn_drop_rate=0.0)
         model.load_param()
         self.model = model
 
