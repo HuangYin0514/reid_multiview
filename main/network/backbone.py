e42dce166fb383be055de0458cb4a7e83e711661
hy1071324110 <hy1071324110@gmail.com>
2025-02-23 17:44:23 +0800

    v421

--- main/network/backbone.py
+++ main/network/backbone.py
@@ -1,5 +1,3 @@
-import copy
-
 import torch
 import torch.nn as nn
 
@@ -9,14 +7,9 @@ from .net_module import resnet50, resnet50_ibn_a, vit_base_patch16_224_TransReID
 class Backbone(nn.Module):
     def __init__(self):
         super(Backbone, self).__init__()
-        model = vit_base_patch16_224_TransReID(img_size=[256, 128], sie_xishu=3.0, local_feature=True, camera=0, view=0, stride_size=[16, 16], drop_path_rate=0.1, drop_rate=0.0, attn_drop_rate=0.0)
+        model = vit_base_patch16_224_TransReID(img_size=[256, 128], sie_xishu=3.0, local_feature=False, camera=0, view=0, stride_size=[16, 16], drop_path_rate=0.1, drop_rate=0.0, attn_drop_rate=0.0)
         model.load_param()
         self.model = model
-
-        block = self.model.blocks[-1]
-        layer_norm = self.model.norm
-        self.b1 = nn.Sequential(copy.deepcopy(block), copy.deepcopy(layer_norm))
-        self.b2 = nn.Sequential(copy.deepcopy(block), copy.deepcopy(layer_norm))
 
     def forward(self, x):
         output = self.model(x, None, None)
--- main/network/model.py
+++ main/network/model.py
@@ -8,7 +8,6 @@ from .net_module import (
     FeatureReconstruction,
     FeatureVectorIntegrationNet,
     GeneralizedMeanPoolingP,
-    shuffle_unit,
 )
 
 
@@ -23,93 +22,14 @@ class Model(nn.Module):
 
         ####################################
         # Classifer [bn -> classifier]
-        in_planes = 768
-        self.backbone_classifier = Classifier(in_planes, config.pid_num)
-
-        ####################################
-        # JPM branch
-        self.local_classifier_1 = Classifier(in_planes, config.pid_num)
-        self.local_classifier_2 = Classifier(in_planes, config.pid_num)
-        self.local_classifier_3 = Classifier(in_planes, config.pid_num)
-        self.local_classifier_4 = Classifier(in_planes, config.pid_num)
-        self.shuffle_groups = 2
-        print("using shuffle_groups size:{}".format(self.shuffle_groups))
-        self.shift_num = 5
-        print("using shift_num size:{}".format(self.shift_num))
-        self.divide_length = 4
-        print("using divide_length size:{}".format(self.divide_length))
+        self.backbone_classifier = Classifier(768, config.pid_num)
 
     def forward(self, x):
         if self.training:
             features = self.backbone(x)
-
-            b1_feat = self.backbone.b1(features)  # [64, 129, 768]
-            global_feat = b1_feat[:, 0]  # [64, 768]
-
-            # JPM branch
-            feature_length = features.size(1) - 1
-            patch_length = feature_length // self.divide_length
-            token = features[:, 0:1]
-            x = shuffle_unit(features, self.shift_num, self.shuffle_groups)
-
-            # Transformer of Last Layer
-            # lf_1
-            b1_local_feat = x[:, :patch_length]
-            b1_local_feat = self.backbone.b2(torch.cat((token, b1_local_feat), dim=1))
-            local_feat_1 = b1_local_feat[:, 0]
-            # lf_2
-            b2_local_feat = x[:, patch_length : patch_length * 2]
-            b2_local_feat = self.backbone.b2(torch.cat((token, b2_local_feat), dim=1))
-            local_feat_2 = b2_local_feat[:, 0]
-            # lf_3
-            b3_local_feat = x[:, patch_length * 2 : patch_length * 3]
-            b3_local_feat = self.backbone.b2(torch.cat((token, b3_local_feat), dim=1))
-            local_feat_3 = b3_local_feat[:, 0]
-            # lf_4
-            b4_local_feat = x[:, patch_length * 3 : patch_length * 4]
-            b4_local_feat = self.backbone.b2(torch.cat((token, b4_local_feat), dim=1))
-            local_feat_4 = b4_local_feat[:, 0]
-
-            return global_feat, local_feat_1, local_feat_2, local_feat_3, local_feat_4
+            return features
         else:
             ###############
-            # features = self.backbone(x)
-            # backbone_bn_features, backbone_cls_score = self.backbone_classifier(features)
             features = self.backbone(x)
-
-            b1_feat = self.backbone.b1(features)  # [64, 129, 768]
-            global_feat = b1_feat[:, 0]  # [64, 768]
-
-            # JPM branch
-            feature_length = features.size(1) - 1
-            patch_length = feature_length // self.divide_length
-            token = features[:, 0:1]
-            x = shuffle_unit(features, self.shift_num, self.shuffle_groups)
-
-            # Transformer of Last Layer
-            # lf_1
-            b1_local_feat = x[:, :patch_length]
-            b1_local_feat = self.backbone.b2(torch.cat((token, b1_local_feat), dim=1))
-            local_feat_1 = b1_local_feat[:, 0]
-            # lf_2
-            b2_local_feat = x[:, patch_length : patch_length * 2]
-            b2_local_feat = self.backbone.b2(torch.cat((token, b2_local_feat), dim=1))
-            local_feat_2 = b2_local_feat[:, 0]
-            # lf_3
-            b3_local_feat = x[:, patch_length * 2 : patch_length * 3]
-            b3_local_feat = self.backbone.b2(torch.cat((token, b3_local_feat), dim=1))
-            local_feat_3 = b3_local_feat[:, 0]
-            # lf_4
-            b4_local_feat = x[:, patch_length * 3 : patch_length * 4]
-            b4_local_feat = self.backbone.b2(torch.cat((token, b4_local_feat), dim=1))
-            local_feat_4 = b4_local_feat[:, 0]
-
-            backbone_bn_features, _ = self.backbone_classifier(global_feat)
-            local_feat_1_bn, _ = self.local_classifier_1(local_feat_1)
-            local_feat_2_bn, _ = self.local_classifier_2(local_feat_2)
-            local_feat_3_bn, _ = self.local_classifier_3(local_feat_3)
-            local_feat_4_bn, _ = self.local_classifier_4(local_feat_4)
-
-            infer_feats = torch.cat([backbone_bn_features, local_feat_1_bn / 4, local_feat_2_bn / 4, local_feat_3_bn / 4, local_feat_4_bn / 4], dim=1)
-
-            return infer_feats
+            backbone_bn_features, backbone_cls_score = self.backbone_classifier(features)
+            return backbone_bn_features
