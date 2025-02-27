41b93ede3634d88b358ec94385074c26fbc0e1d9
hy1071324110 <hy1071324110@gmail.com>
2025-02-27 10:17:41 +0800

    v426

--- main/core/train.py
+++ main/core/train.py
@@ -35,8 +35,7 @@ def train(base, loaders, config):
             localized_features_map = FeatureMapLocation(config).__call__(features_map, pids, base.model.module.backbone_classifier)
 
             localized_features = base.model.module.intergarte_pooling(localized_features_map).squeeze()  # Pooling 池化
-
-            # _, localized_cls_score = base.model.module.backbone_classifier(localized_features)  # Quantization 量化
+            base.model.module.backbone_classifier(localized_features)  # Quantization 量化
 
             # Decoupling
             shared_features, specific_features = base.model.module.featureDecoupling(localized_features)
