43fe9ac9a43ee3b5bc987a82b50797fb0249970a
hy1071324110 <hy1071324110@gmail.com>
2025-03-02 15:32:44 +0800

    v433

--- main/network/net_module/__init__.py
+++ main/network/net_module/__init__.py
@@ -14,4 +14,3 @@ from .MVIIP_processing import (
 from .resnet50 import resnet50
 from .resnet_ibn_a import resnet50_ibn_a
 from .weights_init import weights_init_classifier, weights_init_kaiming
-from .seam import SEAM
