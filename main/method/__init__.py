e38baa1da175258cf2fffc2996590747e40d5b44
hy1071324110 <hy1071324110@gmail.com>
2025-03-06 03:02:00 +0800

    v444

--- main/method/__init__.py
+++ main/method/__init__.py
@@ -1,2 +1,2 @@
-from . import evaluation, loss_function, module, scheduler
+from . import evaluation, innovation, loss_function, module, scheduler
 from .model import *
--- main/method/loss_function/__init__.py
+++ main/method/loss_function/__init__.py
@@ -1,3 +1,2 @@
 from .cross_entropy import CrossEntropyLabelSmooth
-from .regularization import FeatureRegularizationLoss
 from .triplet import TripletLoss
