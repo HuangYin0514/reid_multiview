06d255088b6757e5cb289dbe7ec4683fc4403d9f
hy1071324110 <hy1071324110@gmail.com>
2025-03-13 01:47:47 +0800

    v468

--- main/core/train.py
+++ main/core/train.py
@@ -24,7 +24,8 @@ def train(base, loaders, config):
 
             #################################################################
             # P: Positioning
-            localized_features_map = innovation.multi_view.FeatureMapLocation(config).__call__(features_map, pids, base.model.module.backbone_classifier)
+            new_features_map = features_map.clone().detach()
+            localized_features_map = innovation.multi_view.FeatureMapLocation(config).__call__(new_features_map, pids, base.model.module.backbone_classifier)
 
             # F: Fusion
             intergarte_features = base.model.module.intergarte_gap(localized_features_map).squeeze()
--- main/method/innovation/multi_view.py
+++ main/method/innovation/multi_view.py
@@ -61,16 +61,9 @@ class ContrastLoss:
         self.config = config
 
     def __call__(self, features_1, features_2):
-        new_features_2 = torch.zeros(features_1.size()).to(features_1.device)
-        for i in range(int(features_2.size(0) / 4)):
-            new_features_2[i * 4 : i * 4 + 4] = features_2[i]
+        new_features_2 = torch.repeat_interleave(features_2, repeats=4, dim=0).clone().detach()
         loss = torch.norm((features_1 - new_features_2), p=2)
         return loss
-
-    # def __call__(self, features_1, features_2):
-    #     new_features_2 = torch.repeat_interleave(features_2, repeats=4, dim=0).clone().detach()
-    #     loss = torch.norm((features_1 - new_features_2), p=2)
-    #     return loss
 
     # def v1(self, features_1, features_2):
     #     # v463
