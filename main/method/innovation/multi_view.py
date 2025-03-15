c1caf2a212e1c5365aae6a9c173a196f1e3e0923
hy1071324110 <hy1071324110@gmail.com>
2025-03-15 15:36:20 +0800

    v478

--- main/method/innovation/multi_view.py
+++ main/method/innovation/multi_view.py
@@ -59,16 +59,16 @@ class ContrastLoss:
     #     loss = torch.norm((features_1 - new_features_2), p=2)
     #     return loss
 
-    def __call__(self, features_1, features_2):
-        new_features_2 = torch.zeros(features_1.size()).to(features_1.device)
-        for i in range(int(features_2.size(0) / 4)):
-            new_features_2[i * 4 : i * 4 + 4] = features_2[i]
-        loss = torch.norm((features_1 - new_features_2), p=2)
-        return loss
-
     # def __call__(self, features_1, features_2):
-    #     loss = torch.norm(features_1, p=2)
+    #     new_features_2 = torch.zeros(features_1.size()).to(features_1.device)
+    #     for i in range(int(features_2.size(0) / 4)):
+    #         new_features_2[i * 4 : i * 4 + 4] = features_2[i]
+    #     loss = torch.norm((features_1 - new_features_2), p=2)
     #     return loss
+
+    def __call__(self, features_1, features_2):
+        loss = torch.norm(features_1, p=2)
+        return loss
 
     # def __call__(self, features_1, features_2):
     #     new_features_2 = torch.zeros(features_1.size()).to(features_1.device)
