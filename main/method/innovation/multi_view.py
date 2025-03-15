20da23ee26cff6c4bdf85fc82f75c0663dd40e6a
hy1071324110 <hy1071324110@gmail.com>
2025-03-15 11:07:23 +0800

    v476

--- main/method/innovation/multi_view.py
+++ main/method/innovation/multi_view.py
@@ -60,26 +60,23 @@ class ContrastLoss:
         super(ContrastLoss, self).__init__()
         self.config = config
 
-    def __call__(self, features_1, features_2):
-        new_features_2 = torch.repeat_interleave(features_2, repeats=4, dim=0).clone().detach()
-        loss = torch.norm((features_1 - new_features_2), p=2)
-        return loss
+    # def __call__(self, features_1, features_2):
+    #     new_features_2 = torch.repeat_interleave(features_2, repeats=4, dim=0).clone().detach()
+    #     loss = torch.norm((features_1 - new_features_2), p=2)
+    #     return loss
 
     # def __call__(self, features_1, features_2):
-    #     # v463
     #     new_features_2 = torch.zeros(features_1.size()).to(features_1.device)
     #     for i in range(int(new_features_2.size(0) / 4)):
     #         new_features_2[i * 4 : i * 4 + 4] = features_2[i]
     #     loss = torch.norm((features_1 - new_features_2), p=2)
     #     return loss
 
-    # def __call__(self, features_1, features_2):
-    #     # v462
-    #     loss = torch.norm(features_1, p=2)
-    #     return loss
+    def __call__(self, features_1, features_2):
+        loss = torch.norm(features_1, p=2)
+        return loss
 
     # def __call__(self, features_1, features_2):
-    #     # v465
     #     new_features_2 = torch.zeros(features_1.size()).to(features_1.device)
     #     for i in range(int(features_2.size(0))):
     #         new_features_2[i * 4 : i * 4 + 1] = features_2[i]
