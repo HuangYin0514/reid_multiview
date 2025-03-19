6479cab340dd6bae3a8ebf97f993bf9bbf6d9d9e
hy1071324110 <hy1071324110@gmail.com>
2025-03-20 02:19:26 +0800

    v495

--- main/method/innovation/__init__.py
+++ main/method/innovation/__init__.py
@@ -1,1 +1,11 @@
-from . import decoupling, lct, multi_view, simam, sk_module, srm, triplet_attention
+from . import (
+    decoupling,
+    gc_module,
+    gct,
+    lct,
+    multi_view,
+    simam,
+    sk_module,
+    srm,
+    triplet_attention,
+)
