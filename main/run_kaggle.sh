f2ae5a7b56fed5b2a7d784e9e0b5fd396ce8f623
hy1071324110 <hy1071324110@gmail.com>
2025-03-26 17:12:48 +0800

    v540

--- main/run_kaggle.sh
+++ main/run_kaggle.sh
@@ -6,7 +6,7 @@ wandb login c74133df8c2cf575304acf8a99fe03ab74b6fe6a
 ###########################################################
 # Runs
 ###########################################################
-python main.py --task_name v539 --notes R-I-PDF-C --tags dev occluded_duke --mode train --output_path results/occluded_duke --occluded_duke_path /kaggle/input/occluded-duke/Occluded_Duke --pid_num 702 --module Lucky 
+python main.py --task_name v540 --notes R-I-PDF-C --tags dev occluded_duke --mode train --output_path results/occluded_duke --occluded_duke_path /kaggle/input/occluded-duke/Occluded_Duke --pid_num 702 --module Lucky 
 
 
 # python main.py --task_name test --notes mac实验新方法 --tags mac dev resnet50 occluded_duke --mode train --output_path results/occluded_duke --occluded_duke_path /kaggle/input/occluded-duke/Occluded_Duke --pid_num 702 --module Lucky 
