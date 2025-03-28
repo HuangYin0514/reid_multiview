f7fe808018bfb5d5733e6fa6b1108975e0be1cce
hy1071324110 <hy1071324110@gmail.com>
2025-03-29 00:21:22 +0800

    v566

--- main/method/innovation/multi_view.py
+++ main/method/innovation/multi_view.py
@@ -28,11 +28,12 @@ class ContrastLoss:
     def __call__(self, pred, label, pids):
         # pred: 2D matrix (batch_size, num_classes)
         # label: 1D vector indicating class number
+        bs = pred.size(0)
         T = 3
 
         predict = F.log_softmax(pred / T, dim=1)
         target_data = F.softmax(label / T, dim=1)
         target_data = target_data + 10 ** (-7)
         target = target_data.clone().detach()
-        loss = T * T * ((target * (target.log() - predict)).sum(1).sum() / target.size()[0])
+        loss = 1 / bs * (target * (target.log() - predict)).sum(1).sum()
         return loss
