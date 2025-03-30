47b422fa8cb01e1a61c42ae84999d42d813d27c2
hy1071324110 <hy1071324110@gmail.com>
2025-03-30 22:52:29 +0800

    R_I_F

--- main/method/innovation/multi_view.py
+++ main/method/innovation/multi_view.py
@@ -1,24 +1,23 @@
 import torch
 
+# class FeatureFusion:
+#     def __init__(self, config):
+#         super(FeatureFusion, self).__init__()
+#         self.config = config
 
-class FeatureFusion:
-    def __init__(self, config):
-        super(FeatureFusion, self).__init__()
-        self.config = config
+#     def __call__(self, features, pids):
+#         size = features.size(0)
+#         chunk_size = int(size / 4)  # 16
+#         c = features.size(1)
 
-    def __call__(self, features, pids):
-        size = features.size(0)
-        chunk_size = int(size / 4)  # 16
-        c = features.size(1)
+#         integrating_features = torch.zeros([chunk_size, c]).to(features.device)
+#         integrating_pids = torch.zeros([chunk_size], dtype=torch.long).to(pids.device)
 
-        integrating_features = torch.zeros([chunk_size, c]).to(features.device)
-        integrating_pids = torch.zeros([chunk_size], dtype=torch.long).to(pids.device)
+#         for i in range(chunk_size):
+#             integrating_features[i] = 1 * (features[4 * i] + features[4 * i + 1] + features[4 * i + 2] + features[4 * i + 3])
+#             integrating_pids[i] = pids[4 * i]
 
-        for i in range(chunk_size):
-            integrating_features[i] = 1 * (features[4 * i] + features[4 * i + 1] + features[4 * i + 2] + features[4 * i + 3])
-            integrating_pids[i] = pids[4 * i]
-
-        return integrating_features, integrating_pids
+#         return integrating_features, integrating_pids
 
 
 class FeatureFusion:
