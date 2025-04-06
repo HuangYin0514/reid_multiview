761aa1a5bff96577ea9e9f9ffb90889b3b3d16d8
hy1071324110 <hy1071324110@gmail.com>
2025-04-06 22:11:32 +0800

    R_H2GP_M2F-v615

--- main/core/train.py
+++ main/core/train.py
@@ -45,7 +45,7 @@ def train(base, loaders, config):
             total_loss += hard_part_loss
 
             # ------------- Multiview content branch  -----------------------
-            multiview_global_embedding_feature_maps = base.model.module.multiview_global_embedding(soft_features_l4)
+            multiview_global_embedding_feature_maps = base.model.module.multiview_global_embedding(hard_features)
             multiview_global_embedding_features = base.model.module.multiview_global_pooling(multiview_global_embedding_feature_maps).squeeze()
             multiview_global_fusion_features, multiview_global_fusion_pids = base.model.module.multiview_global_feature_fusion(multiview_global_embedding_features, pids)
             multiview_global_fusion_embedding_features = base.model.module.multiview_global_fusion_embedding(multiview_global_fusion_features.unsqueeze(2).unsqueeze(3)).squeeze()
