01641f8e5e7c102ee54b6439fa4ec3c2f743c615
hy1071324110 <hy1071324110@gmail.com>
2025-04-09 10:52:39 +0800

    R_M2QF_C-v637

--- main/method/innovation/multi_view.py
+++ main/method/innovation/multi_view.py
@@ -16,7 +16,7 @@ class MultiviewFeatureFusion(nn.Module):
 
         # Reshape: [batch_size, C] -> [chunk_size, view_num, C] -> [chunk_size, C]
         fused = features.view(chunk_size, self.view_num, C)
-        fusion_features = fused.mean(dim=1)  # or .mean(dim=1)
+        fusion_features = fused.mean(dim=1)  # or .sum(dim=1)
 
         # 保留每组的第一个pid [size] -> [chunk_size]
         fusion_pids = pids.view(chunk_size, self.view_num)[:, 0]
@@ -107,5 +107,5 @@ class ContrastLoss(nn.Module):
         new_features_2 = torch.zeros(features_1.size()).to(features_1.device)
         for i in range(int(features_1.size(0) / 4)):
             new_features_2[i * 4 : i * 4 + 4] = features_2[i]
-        loss = 0.007 * torch.norm((features_1 - new_features_2), p=2)
+        loss = 0.0007 * torch.norm((features_1 - new_features_2), p=2)
         return loss
