9a227778d7b18f1ba9598f53d11365f3ab154547
hy1071324110 <hy1071324110@gmail.com>
2025-04-11 11:01:57 +0800

    R_M2PQF_C-v655

--- main/method/model.py
+++ main/method/model.py
@@ -9,32 +9,25 @@ class Model(nn.Module):
 
     def __init__(self, config):
         super(Model, self).__init__()
-
-        # ------------- Parameter -----------------------
         self.config = config
         BACKBONE_FEATURES_DIM = config.MODEL.BACKBONE_FEATURES_DIM
         VIEW_NUM = config.MODEL.VIEW_NUM
-
         PID_NUM = config.DATASET.PID_NUM
-
-        EMBEDDING_FEATURES_DIM = config.MODEL.EMBEDDING_FEATURES_DIM
-        PART_NUM = config.MODEL.PART_NUM
 
         # ------------- Backbone -----------------------
         self.backbone = Backbone()
 
         # ------------- Hard content branch -----------------------
         # Global
-        self.hard_global_embedding = module.embedding.Embedding(BACKBONE_FEATURES_DIM, EMBEDDING_FEATURES_DIM)
-        self.hard_global_pooling = module.GeneralizedMeanPoolingP()
-        self.hard_global_classifier = module.Classifier(EMBEDDING_FEATURES_DIM, PID_NUM)
+        self.global_pooling = module.GeneralizedMeanPoolingP()
+        self.global_classifier = module.Classifier(BACKBONE_FEATURES_DIM, PID_NUM)
 
         # ------------- Multiview content branch -----------------------
         self.multiview_pooling = module.GeneralizedMeanPoolingP()
         self.multiview_feature_map_location = innovation.multi_view.FeatureMapLocation()
         self.multiview_feature_quantification = innovation.multi_view.FeatureQuantification(VIEW_NUM)
         self.multiview_feature_fusion = innovation.multi_view.MultiviewFeatureFusion(VIEW_NUM)
-        self.multiview_classifier = module.Classifier(EMBEDDING_FEATURES_DIM, PID_NUM)
+        self.multiview_classifier = module.Classifier(BACKBONE_FEATURES_DIM, PID_NUM)
         self.contrast_loss = innovation.multi_view.ContrastLoss(VIEW_NUM)
 
     def heatmap(self, x):
@@ -48,12 +41,9 @@ class Model(nn.Module):
             eval_features = []
             resnet_feature_maps = self.backbone(x)
 
-            # ------------- Hard content branch -----------------------
-            # Global
-            hard_global_embedding_features = self.hard_global_embedding(resnet_feature_maps)
-            hard_global_pooling_features = self.hard_global_pooling(hard_global_embedding_features).squeeze()
-            hard_global_bn_features, hard_global_cls_score = self.hard_global_classifier(hard_global_pooling_features)
-            eval_features.append(hard_global_bn_features)
+            global_features = self.global_pooling(resnet_feature_maps).squeeze()
+            global_bn_features, global_cls_score = self.global_classifier(global_features)
+            eval_features.append(global_bn_features)
 
             eval_features = torch.cat(eval_features, dim=1)
 
