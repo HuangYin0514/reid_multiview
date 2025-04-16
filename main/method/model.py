d766f78f705e8b61dcd76465d49debf3e92ad962
hy1071324110 <hy1071324110@gmail.com>
2025-04-16 16:43:31 +0800

    R_H2GP_M2PQF_C2Mkl-v686-复现

--- main/method/model.py
+++ main/method/model.py
@@ -26,12 +26,10 @@ class Model(nn.Module):
 
         # Part
         self.hard_part_pooling = nn.ModuleList()
-        self.hard_part_embedding = nn.ModuleList()
         self.hard_part_classifier = nn.ModuleList()
         for i in range(PART_NUM):
             self.hard_part_pooling.append(module.GeneralizedMeanPoolingP())
-            self.hard_part_embedding.append(module.embedding.FCEmbedding(BACKBONE_FEATURES_DIM, EMBEDDING_FEATURES_DIM))
-            self.hard_part_classifier.append(module.Classifier(EMBEDDING_FEATURES_DIM, PID_NUM))
+            self.hard_part_classifier.append(module.Classifier(BACKBONE_FEATURES_DIM, PID_NUM))
 
         # ------------- Multiview content branch -----------------------
         self.multiview_pooling = module.GeneralizedMeanPoolingP()
