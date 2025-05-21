9959b7ee0b15f815245e6211a8c176da9eadd12c
hy1071324110 <hy1071324110@gmail.com>
2025-05-21 17:59:34 +0800

    R_H2GP_S2GA_M2PQF_C2Mkl-v741-局部+多视角+注意力+蒸馏, 注意力分支修复

--- main/method/innovation/dualscale_attention.py
+++ main/method/innovation/dualscale_attention.py
@@ -69,58 +69,58 @@ class Bap(nn.Module):
         return AiF_features, bap_features
 
 
-class ASPP(nn.Module):
-    """
-    Atrous spatial pyramid pooling (ASPP)
-    """
+# class ASPP(nn.Module):
+#     """
+#     Atrous spatial pyramid pooling (ASPP)
+#     """
 
-    def __init__(self, in_channels=2048):
-        super(ASPP, self).__init__()
+#     def __init__(self, in_channels=2048):
+#         super(ASPP, self).__init__()
 
-        atrous_rates = [1, 6, 12]
-        kernels = [3, 3, 3]
-        hidden_dim = 512
+#         atrous_rates = [1, 6, 12]
+#         kernels = [3, 3, 3]
+#         hidden_dim = 512
 
-        self.reduce_dim_conv_1 = nn.Conv2d(in_channels, hidden_dim, 1, 1, 0, bias=False)
+#         self.reduce_dim_conv_1 = nn.Conv2d(in_channels, hidden_dim, 1, 1, 0, bias=False)
 
-        self.conv_layers = nn.ModuleList()
-        for i in range(len(atrous_rates)):
-            rate = atrous_rates[i]
-            kernel = kernels[i]
-            self.conv_layers.append(nn.Conv2d(hidden_dim, hidden_dim, kernel, 1, padding=rate, dilation=rate, bias=True))
+#         self.conv_layers = nn.ModuleList()
+#         for i in range(len(atrous_rates)):
+#             rate = atrous_rates[i]
+#             kernel = kernels[i]
+#             self.conv_layers.append(nn.Conv2d(hidden_dim, hidden_dim, kernel, 1, padding=rate, dilation=rate, bias=True))
 
-        self.conv1 = nn.Conv2d(hidden_dim * 3, in_channels, 1, 1, 0, bias=False)
+#         self.conv1 = nn.Conv2d(hidden_dim * 3, in_channels, 1, 1, 0, bias=False)
 
-        self._initialize_weights()
+#         self._initialize_weights()
 
-    def forward(self, x):
+#     def forward(self, x):
 
-        out = self.reduce_dim_conv_1(x)
+#         out = self.reduce_dim_conv_1(x)
 
-        out_list = []
-        for i in range(len(self.conv_layers)):
-            out_i = self.conv_layers[i](out)
-            out_list.append(out_i)
-        out = torch.cat(out_list, dim=1)
+#         out_list = []
+#         for i in range(len(self.conv_layers)):
+#             out_i = self.conv_layers[i](out)
+#             out_list.append(out_i)
+#         out = torch.cat(out_list, dim=1)
 
-        out = self.conv1(out)
+#         out = self.conv1(out)
 
-        return out
+#         return out
 
-    def _initialize_weights(self):
-        for m in self.modules():
-            if isinstance(m, (nn.Conv1d, nn.Conv2d, nn.Conv3d)):
-                nn.init.kaiming_normal_(m.weight, mode="fan_in")
-                if m.bias is not None:
-                    nn.init.constant_(m.bias, 0)
-            elif isinstance(m, nn.Linear):
-                nn.init.kaiming_normal_(m.weight, mode="fan_out")
-                if m.bias is not None:
-                    nn.init.constant_(m.bias, 0)
-            elif isinstance(m, (nn.BatchNorm1d, nn.BatchNorm2d, nn.BatchNorm3d)):
-                if m.affine:
-                    nn.init.constant_(m.weight, 1)
-                    nn.init.constant_(m.bias, 0)
+#     def _initialize_weights(self):
+#         for m in self.modules():
+#             if isinstance(m, (nn.Conv1d, nn.Conv2d, nn.Conv3d)):
+#                 nn.init.kaiming_normal_(m.weight, mode="fan_in")
+#                 if m.bias is not None:
+#                     nn.init.constant_(m.bias, 0)
+#             elif isinstance(m, nn.Linear):
+#                 nn.init.kaiming_normal_(m.weight, mode="fan_out")
+#                 if m.bias is not None:
+#                     nn.init.constant_(m.bias, 0)
+#             elif isinstance(m, (nn.BatchNorm1d, nn.BatchNorm2d, nn.BatchNorm3d)):
+#                 if m.affine:
+#                     nn.init.constant_(m.weight, 1)
+#                     nn.init.constant_(m.bias, 0)
 
 
 class Dualscale_Attention(nn.Module):
@@ -129,7 +129,6 @@ class Dualscale_Attention(nn.Module):
 
         self.attention_num = attention_num
 
-        self.aspp = ASPP(in_channels)
         self.attention = BasicConv2d(in_channels, attention_num + 1)
         self.bap = Bap(in_channels, out_channels, attention_num)
 
@@ -138,8 +137,7 @@ class Dualscale_Attention(nn.Module):
         return attention[:, : self.attention_num]
 
     def forward(self, features):
-        aspp_features = self.aspp(features)
-        attentions = self.attention(aspp_features)
+        attentions = self.attention(features)
         selected_attentions = self.select_attention(attentions)
         bap_AiF_features, bap_features = self.bap(selected_attentions, features)
         return bap_AiF_features, bap_features
