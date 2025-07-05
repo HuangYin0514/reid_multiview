1499e89044f2c1b90f5a71fccf809ac2579df095
hy1071324110 <hy1071324110@gmail.com>
2025-07-05 23:33:20 +0800

    R_H2GP_S2GA_M2PQF_C2Mkl-v807-局部+多视角+注意力+蒸馏, occluded_reid

--- main/run_kaggle.sh
+++ main/run_kaggle.sh
@@ -6,9 +6,18 @@ wandb login c74133df8c2cf575304acf8a99fe03ab74b6fe6a
 ###########################################################
 # Runs
 ###########################################################
-python main.py --config_file "config/method.yml" 
+# python main.py --config_file "config/method.yml" 
 
-# python main.py --task_name test --notes mac实验新方法 --tags mac dev resnet50 occluded_duke --mode train --output_path results/occluded_duke --occluded_duke_path /kaggle/input/occluded-duke/Occluded_Duke --pid_num 702 --module Lucky 
+python main.py --config_file "config/occluded_reid/0.yml" 
+python main.py --config_file "config/occluded_reid/1.yml" 
+python main.py --config_file "config/occluded_reid/2.yml" 
+python main.py --config_file "config/occluded_reid/3.yml" 
+python main.py --config_file "config/occluded_reid/4.yml"
+python main.py --config_file "config/occluded_reid/5.yml"
+python main.py --config_file "config/occluded_reid/6.yml"
+python main.py --config_file "config/occluded_reid/7.yml"
+python main.py --config_file "config/occluded_reid/8.yml"
+python main.py --config_file "config/occluded_reid/9.yml"
 
 ###########################################################
 # Visualize
