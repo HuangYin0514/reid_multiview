cefae1e2d1fc6c5cd098a1a95e9a748e63575921
hy1071324110 <hy1071324110@gmail.com>
2025-07-09 15:27:05 +0800

    R_H2GP_S2GA_M2PQF_C2Mkl-v813_Hard_Soft

--- main/core/train.py
+++ main/core/train.py
@@ -17,23 +17,23 @@ def train(base, loaders, config):
             # R: Resnet
             resnet_featuremaps, copy_resnet_featuremaps, resnet_internal_featuremaps = base.model(imgs)
 
-            # # ------------- Hard content branch -----------------------
-            # # Global
-            # global_features = base.model.module.global_pooling(resnet_featuremaps).squeeze()
-            # global_bn_features, global_cls_score = base.model.module.global_classifier(global_features)
-            # global_pid_loss = loss_function.CrossEntropyLabelSmooth().forward(global_cls_score, pids)
-            # total_loss += global_pid_loss
+            # ------------- Hard content branch -----------------------
+            # Global
+            global_features = base.model.module.global_pooling(resnet_featuremaps).squeeze()
+            global_bn_features, global_cls_score = base.model.module.global_classifier(global_features)
+            global_pid_loss = loss_function.CrossEntropyLabelSmooth().forward(global_cls_score, pids)
+            total_loss += global_pid_loss
 
-            # # Part
-            # PART_NUM = config.MODEL.PART_NUM
-            # hard_part_chunk_features = torch.chunk(resnet_featuremaps, PART_NUM, dim=2)
-            # hard_part_pid_loss = 0.0
-            # for i in range(PART_NUM):
-            #     hard_part_chunk_feature_item = hard_part_chunk_features[i]
-            #     hard_part_pooling_features = base.model.module.hard_part_pooling[i](hard_part_chunk_feature_item).squeeze()
-            #     hard_part_bn_features, hard_part_cls_score = base.model.module.hard_part_classifier[i](hard_part_pooling_features)
-            #     hard_part_pid_loss += (1 / PART_NUM) * loss_function.CrossEntropyLabelSmooth().forward(hard_part_cls_score, pids)
-            # total_loss += hard_part_pid_loss
+            # Part
+            PART_NUM = config.MODEL.PART_NUM
+            hard_part_chunk_features = torch.chunk(resnet_featuremaps, PART_NUM, dim=2)
+            hard_part_pid_loss = 0.0
+            for i in range(PART_NUM):
+                hard_part_chunk_feature_item = hard_part_chunk_features[i]
+                hard_part_pooling_features = base.model.module.hard_part_pooling[i](hard_part_chunk_feature_item).squeeze()
+                hard_part_bn_features, hard_part_cls_score = base.model.module.hard_part_classifier[i](hard_part_pooling_features)
+                hard_part_pid_loss += (1 / PART_NUM) * loss_function.CrossEntropyLabelSmooth().forward(hard_part_cls_score, pids)
+            total_loss += hard_part_pid_loss
 
             # ------------- Soft content branch -----------------------
             # Soft global
@@ -74,7 +74,7 @@ def train(base, loaders, config):
             # multiview_pid_loss = loss_function.CrossEntropyLabelSmooth().forward(multiview_cls_score, multiview_pids)
             # total_loss += multiview_pid_loss
 
-            # ------------- ContrastLoss  -----------------------
+            # # ------------- ContrastLoss  -----------------------
             # # contrast_loss = base.model.module.contrast_loss(global_bn_features, multiview_bn_features)
             # contrast_loss = base.model.module.contrast_kl_loss(global_cls_score, multiview_cls_score, global_bn_features, multiview_bn_features)
             # contrast_loss_2 = base.model.module.contrast_kl_loss(soft_global_cls_score, multiview_cls_score, soft_global_bn_features, multiview_bn_features)
@@ -86,7 +86,10 @@ def train(base, loaders, config):
 
             meter.update(
                 {
-                    "soft_attention_pid_loss": soft_attention_pid_loss.data,
+                    "global_pid_loss": global_pid_loss.data,
+                    "hard_part_pid_loss": hard_part_pid_loss.data,
+                    # "multiview_pid_loss": multiview_pid_loss.data,
+                    # "contrast_loss": contrast_loss.data,
                 }
             )
 
