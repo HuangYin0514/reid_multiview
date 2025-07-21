29413b259f50706a23a80c05b7e4c20958c703f2
hy1071324110 <hy1071324110@gmail.com>
2025-07-21 16:24:08 +0800

    R_H2GP_S2GA_M2PQF_C2Mkl-v848_R_H2GP_S2GA

--- main/core/train.py
+++ main/core/train.py
@@ -49,36 +49,36 @@ def train(base, loaders, config):
             soft_attention_pid_loss = loss_function.CrossEntropyLabelSmooth().forward(soft_attention_cls_score, pids)
             total_loss += soft_attention_pid_loss
 
-            # ------------- Multiview content branch  -----------------------
-            # Position
-            multiview_hard_CAM_featuremaps = base.model.module.multiview_hard_CAM(resnet_featuremaps, pids, base.model.module.global_classifier)
-            multiview_soft_CAM_featuremaps = base.model.module.multiview_soft_CAM(copy_resnet_featuremaps, pids, base.model.module.soft_global_classifier)
+            # # ------------- Multiview content branch  -----------------------
+            # # Position
+            # multiview_hard_CAM_featuremaps = base.model.module.multiview_hard_CAM(resnet_featuremaps, pids, base.model.module.global_classifier)
+            # multiview_soft_CAM_featuremaps = base.model.module.multiview_soft_CAM(copy_resnet_featuremaps, pids, base.model.module.soft_global_classifier)
 
-            # Featuremaps fusion
-            multiview_featuremaps = base.model.module.multiview_featuremap_fusion(multiview_hard_CAM_featuremaps, multiview_soft_CAM_featuremaps)
+            # # Featuremaps fusion
+            # multiview_featuremaps = base.model.module.multiview_featuremap_fusion(multiview_hard_CAM_featuremaps, multiview_soft_CAM_featuremaps)
 
-            # Quantification
-            multiview_features = base.model.module.multiview_pooling(multiview_featuremaps).squeeze()
-            _, multiview_hard_cls_score = base.model.module.global_classifier(multiview_features)
-            _, multiview_soft_cls_score = base.model.module.soft_global_classifier(multiview_features)
-            # multiview_quantification_cls_score = (multiview_hard_cls_score + multiview_soft_cls_score) / 2
-            multiview_quantification_cls_score = multiview_hard_cls_score + multiview_soft_cls_score
-            multiview_quantification_cls_score = F.softmax(multiview_quantification_cls_score, dim=1)
-            multiview_features = base.model.module.multiview_feature_quantification(multiview_features, multiview_quantification_cls_score, pids)
+            # # Quantification
+            # multiview_features = base.model.module.multiview_pooling(multiview_featuremaps).squeeze()
+            # _, multiview_hard_cls_score = base.model.module.global_classifier(multiview_features)
+            # _, multiview_soft_cls_score = base.model.module.soft_global_classifier(multiview_features)
+            # # multiview_quantification_cls_score = (multiview_hard_cls_score + multiview_soft_cls_score) / 2
+            # multiview_quantification_cls_score = multiview_hard_cls_score + multiview_soft_cls_score
+            # multiview_quantification_cls_score = F.softmax(multiview_quantification_cls_score, dim=1)
+            # multiview_features = base.model.module.multiview_feature_quantification(multiview_features, multiview_quantification_cls_score, pids)
 
-            # View fusion
-            multiview_features, multiview_pids = base.model.module.multiview_view_fusion(multiview_features, pids)
+            # # View fusion
+            # multiview_features, multiview_pids = base.model.module.multiview_view_fusion(multiview_features, pids)
 
-            # Classification
-            multiview_bn_features, multiview_cls_score = base.model.module.multiview_classifier(multiview_features)
-            multiview_pid_loss = loss_function.CrossEntropyLabelSmooth().forward(multiview_cls_score, multiview_pids)
-            total_loss += multiview_pid_loss
+            # # Classification
+            # multiview_bn_features, multiview_cls_score = base.model.module.multiview_classifier(multiview_features)
+            # multiview_pid_loss = loss_function.CrossEntropyLabelSmooth().forward(multiview_cls_score, multiview_pids)
+            # total_loss += multiview_pid_loss
 
-            # ------------- ContrastLoss  -----------------------
-            # contrast_loss = base.model.module.contrast_loss(global_bn_features, multiview_bn_features)
-            contrast_loss = base.model.module.contrast_kl_loss(global_cls_score, multiview_cls_score, global_bn_features, multiview_bn_features)
-            contrast_loss_2 = base.model.module.contrast_kl_loss(soft_global_cls_score, multiview_cls_score, soft_global_bn_features, multiview_bn_features)
-            total_loss += contrast_loss + contrast_loss_2
+            # # ------------- ContrastLoss  -----------------------
+            # # contrast_loss = base.model.module.contrast_loss(global_bn_features, multiview_bn_features)
+            # contrast_loss = base.model.module.contrast_kl_loss(global_cls_score, multiview_cls_score, global_bn_features, multiview_bn_features)
+            # contrast_loss_2 = base.model.module.contrast_kl_loss(soft_global_cls_score, multiview_cls_score, soft_global_bn_features, multiview_bn_features)
+            # total_loss += contrast_loss + contrast_loss_2
 
             base.model_optimizer.zero_grad()
             total_loss.backward()
@@ -88,8 +88,8 @@ def train(base, loaders, config):
                 {
                     "global_pid_loss": global_pid_loss.data,
                     "hard_part_pid_loss": hard_part_pid_loss.data,
-                    "multiview_pid_loss": multiview_pid_loss.data,
-                    "contrast_loss": contrast_loss.data,
+                    # "multiview_pid_loss": multiview_pid_loss.data,
+                    # "contrast_loss": contrast_loss.data,
                 }
             )
 
