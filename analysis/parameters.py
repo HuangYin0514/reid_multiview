369105922cf4d2dc54a1b7373a836a587e5b5f88
hy1071324110 <hy1071324110@gmail.com>
2025-07-28 10:28:38 +0800

    R_H2GP_S2GA_M2PQF_C2Mkl-v860_Baseline_visualization

--- analysis/parameters.py
+++ analysis/parameters.py
@@ -115,16 +115,16 @@ def plot_parameter_viewNum():
 
     # 保存
     # plt.show()
-    output_file = os.path.join(OUTPUT_DIR, "parameter_viewNum.png")
+    output_file = os.path.join(OUTPUT_DIR, "parameter_viewNum_bar.png")
     plt.savefig(output_file, bbox_inches="tight")
     print(f"图像已保存到: {output_file}")
 
 
-def plot_parameter_partNum():
+def plot_parameter_partNum_Bar():
     # 设置数据 --------------------------------------------------------
-    x_values = [2, 4, 8, 16]
-    map_values = [59.1, 58.3, 58.2, 57.4]
-    rank1_values = [71.2, 69.0, 69.3, 69.0]
+    x_values = [1, 2, 4, 8, 16]
+    map_values = [58.6, 59.1, 58.3, 58.2, 57.4]
+    rank1_values = [70.4, 71.2, 69.0, 69.3, 69.0]
 
     # 创建画布 --------------------------------------------------------
     LINE_NUM = 1
@@ -174,13 +174,64 @@ def plot_parameter_partNum():
 
     # 保存
     # plt.show()
-    output_file = os.path.join(OUTPUT_DIR, "parameter_partNum.png")
+    output_file = os.path.join(OUTPUT_DIR, "parameter_partNum_bar.png")
+    plt.savefig(output_file, bbox_inches="tight")
+    print(f"图像已保存到: {output_file}")
+
+
+def plot_parameter_partNum_line():
+    # 设置数据 --------------------------------------------------------
+    x_values = [1, 2, 4, 8, 16]
+    map_values = [58.6, 59.1, 58.3, 58.2, 57.4]
+    rank1_values = [70.4, 71.2, 69.0, 69.3, 69.0]
+
+    # 创建画布 --------------------------------------------------------
+    LINE_NUM = 1
+    ROW_NUM = 1
+    FIGSIZE = (4 * ROW_NUM, 3 * LINE_NUM)
+    fig, axis = plt.subplots(LINE_NUM, ROW_NUM, figsize=FIGSIZE, dpi=LATAX_DPI)
+    axis_1 = axis
+    axis_2 = axis_1.twinx()  #  创建双y轴
+
+    # 图参数
+    max_values = max([max(m, r) for m, r in zip(map_values, rank1_values)])
+    min_values = min([min(m, r) for m, r in zip(map_values, rank1_values)])
+    offset = int((max_values - min_values) * 0.4)
+
+    # 绘制左侧轴
+    axis_1.set_ylim(min_values - offset, max_values - offset)
+    axis_1.set_ylabel("mAP (%)")
+
+    # 绘制右侧轴
+    axis_2.set_ylim(min_values + offset, max_values + offset)
+    axis_2.set_ylabel("Rank-1 (%)")
+
+    # 绘制横轴
+    axis_1.set_xlabel(r"Parameter $\lambda$")
+
+    # 绘制图像
+    axis_1.plot(x_values, map_values, label="mAP", color="#FD625E", marker="o", markersize=MARKERSIZE, zorder=2)
+    axis_2.plot(x_values, rank1_values, label="Rank-1", color="#71D4EB", marker="o", markersize=MARKERSIZE, zorder=2)
+
+    # 图像调整
+    # axis_1.grid(axis="x")
+    axis_2.grid(False)
+    handles1, labels1 = axis_1.get_legend_handles_labels()
+    handles2, labels2 = axis_2.get_legend_handles_labels()
+    axis_2.legend(handles1 + handles2, labels1 + labels2).get_frame().set_alpha(1)
+    plt.tight_layout()
+
+    # 保存
+    # plt.show()
+    output_file = os.path.join(OUTPUT_DIR, "parameter_partNum_line.png")
     plt.savefig(output_file, bbox_inches="tight")
     print(f"图像已保存到: {output_file}")
 
 
 if __name__ == "__main__":
     # Path check
+    if os.path.exists(OUTPUT_DIR):
+        shutil.rmtree(OUTPUT_DIR)  # 删除整个目录及其内容
     OUTPUT_DIR = Path(OUTPUT_DIR)
     OUTPUT_DIR.mkdir(parents=True, exist_ok=True)
 
@@ -191,4 +242,5 @@ if __name__ == "__main__":
     plot_parameter_viewNum()
 
     # parameter of part num
-    plot_parameter_partNum()
+    plot_parameter_partNum_Bar()
+    plot_parameter_partNum_line()
--- main/run_kaggle.sh
+++ main/run_kaggle.sh
@@ -9,19 +9,16 @@ wandb login c74133df8c2cf575304acf8a99fe03ab74b6fe6a
 # python main.py --config_file "config/method.yml" TASK.NOTES=v859_visualization TASK.NAME=R_H2GP_S2GA_M2PQF_C2Mkl
 
 # python main.py --config_file "config/method.yml" TASK.NOTES=v859_visualization TASK.NAME=R_H2GP_S2GA_M2PQF_C2Mkl MODEL.MODULE=Baseline
-
-python main.py --config_file "config/method.yml" TASK.NAME=R_H2GP_S2GA_M2PQF_C2Mkl TASK.NOTES=v859_PART_NUM_5 MODEL.MODULE=Lucky MODEL.PART_NUM=5
-python main.py --config_file "config/method.yml" TASK.NAME=R_H2GP_S2GA_M2PQF_C2Mkl TASK.NOTES=v859_PART_NUM_6 MODEL.MODULE=Lucky MODEL.PART_NUM=6
-
-
-# python main.py --config_file "config/occluded_reid/0.yml" 
-
+python main.py --config_file "config/method.yml" TASK.NAME=R_H2GP_S2GA_M2PQF_C2Mkl TASK.NOTES=v859_PART_NUM_5 MODEL.MODULE=Lucky MODEL.PART_NUM=2
 
 ###########################################################
 # Visualize
 ###########################################################
-# mkdir -p results/outputs/models
-# wget -O results/outputs/models/model_114.pth "https://storage.googleapis.com/kaggle-script-versions/250844267/output/reid_multiview/main/results/outputs/models/model_114.pth?X-Goog-Algorithm=GOOG4-RSA-SHA256&X-Goog-Credential=gcp-kaggle-com%40kaggle-161607.iam.gserviceaccount.com%2F20250727%2Fauto%2Fstorage%2Fgoog4_request&X-Goog-Date=20250727T161254Z&X-Goog-Expires=3600&X-Goog-SignedHeaders=host&X-Goog-Signature=6cedfff84aedfd72ec487df4e50036e2a69b844fa96afbcfecfcafd8b4f5fd5504370b991a1874ac8650050cc3c624769196c2063d6a4f0b60ff8bdd55c804db53651e52e648885f67285a9e54ab3e086e9d120303efe6ca395958dc6c857f69104851e4c1b4669ee616d9ff484c2ccbcda6cd9bb2239a36ef65c8838965606a621f8847ed31d5a371e3e5c6a609496dd0e257d5f3de7278a04073053037af3a6736960021accb207c7204330afe1800430e41dad20e04e2a6033ad856dff1f0238c845ac50095d7db74f174e09cd20ff5210c66e73d5603f38a8452a023e18b68bc7c47c25903f76b273b0047a2dd3e5a9d2bdb21c93bc310d91fe8f8724c09"
-# python main.py --config_file "config/method.yml" TASK.MODE=visualization TEST.RESUME_TEST_MODEL=114 
-# tar -czf results.tar.gz results
-# rm -rf results/outputs/actmap results/outputs/rank results/outputs/tSNE 
+mkdir -p results/outputs/models
+wget -O results/outputs/models/model_114.pth "https://storage.googleapis.com/kaggle-script-versions/251889179/output/reid_multiview/main/results/outputs/models/model_114.pth?X-Goog-Algorithm=GOOG4-RSA-SHA256&X-Goog-Credential=gcp-kaggle-com%40kaggle-161607.iam.gserviceaccount.com%2F20250728%2Fauto%2Fstorage%2Fgoog4_request&X-Goog-Date=20250728T022543Z&X-Goog-Expires=3600&X-Goog-SignedHeaders=host&X-Goog-Signature=acceacce8026b3af8c0934a5f3d446081e23a9f6c2da376d3a9027286ef32eb50f77e0ad3bccc2d240724fe2425b899179be5fb4d443551fc71fb5bc84200b88a50c2dcf836c84c896832746fc75e9a10ab9480bdd6ba5e08c74e77216527b12727af0c2d232a2c4dfb726f091369ee1fc29780616a172ecd0e41ba73a9fbaa89cbb417c8dc8db52133ca16a47e979dd2284e907187f57f3b99c5b0278f7ce05e90d44a820966de3b3dbe59c40cc280da00649cc8dd19f07dbed7089d7a2840b6bf7d7a9989b101840567d11db9fa7d18a73fe8a2a136c37fcfccfd81c90e67b9133ce60396412ae1ba24d239dae5d712d093a66b9b8b634377af0026f21ede5"
+
+# python main.py --config_file "config/method.yml"  TEST.RESUME_TEST_MODEL=114 
+python main.py --config_file "config/method.yml" TASK.NAME=R_H2GP_S2GA_M2PQF_C2Mkl TASK.NOTES=v860_Baseline_visualization TASK.MODE=visualization MODEL.MODULE=Baseline
+
+tar -czf results.tar.gz results
+rm -rf results/outputs/actmap results/outputs/rank results/outputs/tSNE 
